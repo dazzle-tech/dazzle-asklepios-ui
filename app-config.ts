27f78ec75976a5354df1deb18d90d490401d4353
Alaa Banifadel <79154842+alban15@users.noreply.github.com>
2024-10-28 21:42:10 +0100

    Update app-config.ts

--- app-config.ts
+++ app-config.ts
@@ -1,7 +1,7 @@
 const config = {
     backendBaseURL:'https://asklepiosapi.nereuscloud.de',
     tenantId: '1',
-    tenantSecret: '4994'
+    tenantSecurityToken: '4994'
   };
   
   export default config;
