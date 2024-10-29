238ef7fed64905746cd72029350016f3a452b141
hanan253yahya <165596621+hanan253yahya@users.noreply.github.com>
2024-10-29 13:47:24 +0200

    "New design for login screen AS-213"

--- src/pages/authentication/sign-in/SignIn.tsx
+++ src/pages/authentication/sign-in/SignIn.tsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { Form, Button, Panel, SelectPicker, Stack, Divider, Message, Modal, ButtonToolbar } from 'rsuite';
 import { useAppDispatch, useAppSelector } from '@/hooks';
-import Logo from '../../../images/ASK_LOGO.svg';
+import Logo from '../../../images/ASK_LOGO_SVG copy.svg';
+import Background from '../../../images/ASK_WALLPAPER.svg';
+import UserLogo from '../../../images/Login_ICon.svg';
 import Translate from '@/components/Translate';
 import { useLoginMutation } from "@/services/authService";
 import { useNavigate } from "react-router-dom";
@@ -114,32 +116,72 @@ const SignIn = () => {
   }
 
   return (
-    <Stack
-      justifyContent="center"
-      alignItems="center"
-      direction="column"
-      style={{
-        height: '100vh'
-      }}
-    >
-      <img
-        src={
-          authSlice.tenant && authSlice.tenant.tenantLogoPath
-            ? authSlice.tenant.tenantLogoPath
-            : Logo
-        }
-        width={300}
-      />
-      <br />
 
+    <Panel
+    bordered
+    style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      flexDirection: 'column',
+      height: '100vh',
+      borderRadius: '10px',
+      padding: '20px',
+      position: 'relative',
+     
+     overflow: 'hidden'
+    }}
+  >
+     <img 
+    src={Background}
+    alt="Background" 
+    style={{
+      position: 'absolute', // لجعل الصورة في الخلفية
+      top: 0,
+      left: 0,
+      opacity: 0.6 ,
+      width: '100%', // لتغطية العرض بالكامل
+      height: '100%', // لتغطية الارتفاع بالكامل
+      objectFit: 'cover', // لجعل الصورة تغطي كامل الـ Panel
+      zIndex: -1 // لجعل الصورة خلف المحتوى
+    }} 
+  />
+        {/* Logo Panel */}
+        <Panel style={{ width: 550 }}>
+        <img
+          src={authSlice.tenant && authSlice.tenant.tenantLogoPath ? authSlice.tenant.tenantLogoPath : Logo}
+          width={400}
+          alt="Tenant Logo"
+        />
+      </Panel>
+  
+      {/* Sign In Panel */}
       {!resetPasswordView && (
-        <Panel bordered style={{ background: '#fff', width: 400, padding: 20 }}
-          header={<h3>Sign In</h3>}>
+        <Panel bordered style={{    backgroundColor: 'rgba(250, 250, 250, 0.5)', width: 450, padding: 40, borderRadius: '10px'  }}>
+        
+        <img
+    src={UserLogo}
+    alt="Header Background"
+    style={{
+      marginLeft:"98px",
+      top: 50, // يمكنك تعديل هذا لتغيير موضع الصورة
+      left: 50, // يمكنك تعديل هذا لتغيير موضع الصورة
+      width: '127px', // ضبط عرض الصورة
+      height: '110px',
+     // الحفاظ على نسبة العرض إلى الارتفاع
+       // يجب أن تكون الصورة أعلى من المحتوى
+    }}
+  />
+ 
+  <h3 style={{ textAlign: 'center', fontFamily: 'Arial, sans-serif', zIndex: 2 }}>
+    Sign In
+  </h3>
           {!authSlice.tenant && (
             <Message type="warning" showIcon>
               <Translate>No Tenant Configured</Translate>
             </Message>
           )}
+  
           <Form fluid onKeyPress={handleKeyPress}>
             <Form.Group>
               <Form.ControlLabel>Organization</Form.ControlLabel>
@@ -152,24 +194,20 @@ const SignIn = () => {
                 labelKey='facilityName'
                 valueKey='key'
                 value={credentials.orgKey}
-                onChange={e => {
-                  setCredentials({ ...credentials, orgKey: e });
-                }}
+                onChange={e => setCredentials({ ...credentials, orgKey: e })}
               />
             </Form.Group>
-
+  
             <Form.Group>
               <Form.ControlLabel>Username</Form.ControlLabel>
               <Form.Control
                 disabled={!authSlice.tenant}
                 name="username"
                 value={credentials.username}
-                onChange={e => {
-                  setCredentials({ ...credentials, username: e });
-                }}
+                onChange={e => setCredentials({ ...credentials, username: e })}
               />
             </Form.Group>
-
+  
             <Form.Group>
               <Form.ControlLabel>
                 <span>Password</span>
@@ -177,32 +215,31 @@ const SignIn = () => {
               </Form.ControlLabel>
               <Form.Control
                 disabled={!authSlice.tenant}
-                name="passowrd"
+                name="password"
                 type="password"
                 value={credentials.password}
-                onChange={e => {
-                  setCredentials({ ...credentials, password: e });
-                }}
+                onChange={e => setCredentials({ ...credentials, password: e })}
               />
             </Form.Group>
+  
             <Form.Group>
-              <Stack spacing={6} divider={<Divider vertical />}>
-                <Button appearance="primary" onClick={
-                  handleLogin
-
-                } disabled={!authSlice.tenant}>
-                  Sign in
-                </Button>
-              </Stack>
+              <Button color="cyan" appearance="primary" onClick={handleLogin} disabled={!authSlice.tenant}  style={{ 
+      borderRadius: '25px', 
+      padding: '15px 20px', // تغيير حجم الزر
+      fontSize: '16px',
+      width:"100%" // حجم الخط
+       // لون الخلفية
+    }} >
+                Sign in
+              </Button>
             </Form.Group>
           </Form>
-
-
         </Panel>
       )}
-
+  
+      {/* Reset Password Panel */}
       {resetPasswordView && (
-        <Panel bordered style={{ background: '#fff', width: 400 }} header={<h3>Sign In</h3>}>
+        <Panel bordered style={{ background: '#fff', width: 400, padding: 40, borderRadius: '8px' }} header={<h3>Sign In</h3>}>
           <Form fluid>
             <Form.Group>
               <Form.ControlLabel>Organization</Form.ControlLabel>
@@ -212,83 +249,64 @@ const SignIn = () => {
                 name="organization"
                 data={organizations}
                 value={credentials.orgKey}
-                onChange={e => {
-                  setCredentials({ ...credentials, orgKey: e });
-                }}
+                onChange={e => setCredentials({ ...credentials, orgKey: e })}
               />
             </Form.Group>
-
+  
             <Form.Group>
               <Form.ControlLabel>Username</Form.ControlLabel>
               <Form.Control
                 name="username"
                 value={credentials.username}
-                onChange={e => {
-                  setCredentials({ ...credentials, username: e });
-                }}
+                onChange={e => setCredentials({ ...credentials, username: e })}
               />
             </Form.Group>
+  
             <Form.Group>
-              <Stack spacing={6} divider={<Divider vertical />}>
-                <Button appearance="primary" onClick={handleLogin}>
-                  Send OTP
-                </Button>
-              </Stack>
+              <Button appearance="primary" onClick={handleLogin}>
+                Send OTP
+              </Button>
             </Form.Group>
           </Form>
         </Panel>
       )}
-
-
-
-      <Modal backdrop="static" role="alertdialog" open={changePasswordView}
-        // onClose={handleClose}
-        size="xs">
-        <Modal.Body>
-          <RemindIcon style={{ color: '#ffb300', fontSize: 24 }} />
-          {'new password required!'}
-
-
-          <Form fluid>
-
-            <Form.Group>
-              <Form.ControlLabel>New Password</Form.ControlLabel>
-              <Form.Control
-                name="New Password"
-                value={newPassword}
-                onChange={e => {
-                  setNewPassword(e)
-                }}
-              />
-            </Form.Group>
-            <Form.Group>
-              <Form.ControlLabel>Password Confirm</Form.ControlLabel>
-              <Form.Control
-                name="Password Confirm"
-                value={newPasswordConfirm}
-                onChange={e => {
-                  setNewPasswordConfirm(e)
-                }}
-              />
-            </Form.Group>
-          </Form>
-          <p style={{ color: "red" }}> {errText}</p>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button
-            onClick={handleSaveNewPassword}
-            appearance="primary">
-            Ok
-          </Button>
-          <Button
-            // onClick={handleClose}
-            appearance="subtle">
-            Cancel
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    </Stack>
-
+  
+    
+   
+  
+    {/* Modal for Password Change */}
+    <Modal backdrop="static" role="alertdialog" open={changePasswordView} size="xs">
+      <Modal.Body>
+        <RemindIcon style={{ color: '#ffb300', fontSize: 24 }} />
+        {'New password required!'}
+  
+        <Form fluid>
+          <Form.Group>
+            <Form.ControlLabel>New Password</Form.ControlLabel>
+            <Form.Control
+              name="New Password"
+              value={newPassword}
+              onChange={e => setNewPassword(e)}
+            />
+          </Form.Group>
+          <Form.Group>
+            <Form.ControlLabel>Password Confirm</Form.ControlLabel>
+            <Form.Control
+              name="Password Confirm"
+              value={newPasswordConfirm}
+              onChange={e => setNewPasswordConfirm(e)}
+            />
+          </Form.Group>
+        </Form>
+        <p style={{ color: "red" }}> {errText}</p>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={handleSaveNewPassword} appearance="primary">Ok</Button>
+        <Button appearance="subtle">Cancel</Button>
+      </Modal.Footer>
+    </Modal>
+  </Panel>
+  
 
   );
 };
