2a8cbcfabd710600d053838a9dfed7975d7531a9
Ameed Jadallah <ameed.jadallah@gmail.com>
2025-04-19 23:17:43 +0300

    Update EncounterList.tsx

--- src/pages/encounter/encounter-list/EncounterList.tsx
+++ src/pages/encounter/encounter-list/EncounterList.tsx
@@ -21,6 +21,8 @@ import { useDispatch } from 'react-redux';
 import ReactDOMServer from 'react-dom/server';
 import './styles.less';
 import { hideSystemLoader, showSystemLoader } from '@/utils/uiReducerActions';
+import MyTable from '@/components/MyTable';
+
 const EncounterList = () => {
   const location = useLocation();
   const navigate = useNavigate();
@@ -153,6 +155,172 @@ const EncounterList = () => {
     }
   }, [isLoading]);
 
+  const tableColumns = [
+    {
+      key: 'queueNumber',
+      title: <Translate>#</Translate>,
+      flexGrow: 1,
+      dataKey: 'queueNumber'
+    },
+    {
+      key: 'patientFullName',
+      title: <Translate>PATIENT NAME</Translate>,
+      flexGrow: 6,
+      fullText: true,
+      render: rowData => {
+        const tooltipSpeaker = (
+          <Tooltip>
+            <div>MRN : {rowData?.patientObject?.patientMrn}</div>
+            <div>Age : {rowData?.patientAge}</div>
+            <div>
+              Gender :{' '}
+              {rowData?.patientObject?.genderLvalue
+                ? rowData?.patientObject?.genderLvalue?.lovDisplayVale
+                : rowData?.patientObject?.genderLkey}
+            </div>
+            <div>Visit ID : {rowData?.visitId}</div>
+          </Tooltip>
+        );
+
+        return (
+          <Whisper trigger="hover" placement="top" speaker={tooltipSpeaker}>
+            <div style={{ display: 'inline-block' }}>
+              {rowData?.patientObject?.privatePatient ? (
+                <Badge color="cyan" content="Private">
+                  <p style={{ marginTop: '5px', cursor: 'pointer' }}>
+                    {rowData?.patientObject?.fullName}
+                  </p>
+                </Badge>
+              ) : (
+                <p style={{ cursor: 'pointer' }}>{rowData?.patientObject?.fullName}</p>
+              )}
+            </div>
+          </Whisper>
+        );
+      }
+    },
+    {
+      key: 'visitType',
+      title: <Translate>VISIT TYPE</Translate>,
+      flexGrow: 4,
+      render: rowData =>
+        rowData.visitTypeLvalue
+          ? rowData.visitTypeLvalue.lovDisplayVale
+          : rowData.visitTypeLkey
+    },
+    {
+      key: 'chiefComplaint',
+      title: <Translate>CHIEF COMPLAIN</Translate>,
+      flexGrow: 4,
+      render: rowData => rowData.chiefComplaint
+    },
+    {
+      key: 'diagnosis',
+      title: <Translate>DIAGNOSIS</Translate>,
+      flexGrow: 4,
+      render: rowData => rowData.diagnosis
+    },
+    {
+      key: 'hasPrescription',
+      title: <Translate>PRESCRIPTION</Translate>,
+      flexGrow: 3,
+      render: rowData =>
+        rowData.hasPrescription ? (
+          <Badge content="YES" className="status-yes" />
+        ) : (
+          <Badge content="NO" className="status-no" />
+        )
+    },
+    {
+      key: 'hasOrder',
+      title: <Translate>HAS ORDER</Translate>,
+      flexGrow: 3,
+      render: rowData =>
+        rowData.hasOrder ? (
+          <Badge content="YES" className="status-yes" />
+        ) : (
+          <Badge content="NO" className="status-no" />
+        )
+    },
+    {
+      key: 'encounterPriority',
+      title: <Translate>PRIORITY</Translate>,
+      flexGrow: 3,
+      render: rowData =>
+        rowData.encounterPriorityLvalue
+          ? rowData.encounterPriorityLvalue.lovDisplayVale
+          : rowData.encounterPriorityLkey
+    },
+    {
+      key: 'plannedStartDate',
+      title: <Translate>DATE</Translate>,
+      flexGrow: 3,
+      dataKey: 'plannedStartDate'
+    },
+    {
+      key: 'status',
+      title: <Translate>STATUS</Translate>,
+      flexGrow: 3,
+      render: rowData =>
+        rowData.encounterStatusLvalue
+          ? rowData.encounterStatusLvalue.lovDisplayVale
+          : rowData.encounterStatusLkey
+    },
+    {
+      key: 'hasObservation',
+      title: <Translate>IS OBSERVED</Translate>,
+      render: rowData =>
+        rowData.hasObservation ? (
+          <Badge content="YES" className="status-yes" />
+        ) : (
+          <Badge content="NO" className="status-no" />
+        )
+    },
+    {
+      key: 'actions',
+      title: <Translate> </Translate>,
+      render: rowData => {
+        const tooltipNurse = <Tooltip>Nurse Station</Tooltip>;
+        const tooltipDoctor = <Tooltip>Go to Visit</Tooltip>;
+        return (
+          <Form layout="inline" fluid className="nurse-doctor-form">
+            <Whisper trigger="hover" placement="top" speaker={tooltipDoctor}>
+              <div>
+                <MyButton
+                  size="small"
+                  onClick={() => {
+                    const patientData = rowData.patientObject;
+                    setLocalEncounter(rowData);
+                    setLocalPatient(patientData);
+                    handleGoToVisit(rowData, patientData);
+                  }}
+                >
+                  <FontAwesomeIcon icon={faUserDoctor} />
+                </MyButton>
+              </div>
+            </Whisper>
+            <Whisper trigger="hover" placement="top" speaker={tooltipNurse}>
+              <div>
+                <MyButton
+                  size="small"
+                  backgroundColor="black"
+                  onClick={() => {
+                    const patientData = rowData.patientObject;
+                    setLocalEncounter(rowData);
+                    setLocalPatient(patientData);
+                    handleGoToPreVisitObservations(rowData, patientData);
+                  }}
+                >
+                  <FontAwesomeIcon icon={faUserNurse} />
+                </MyButton>
+              </div>
+            </Whisper>
+          </Form>
+        );
+      }
+    }
+  ];
+
   return (
     <Panel>
       <Form layout="inline" fluid className="date-filter-form">
@@ -182,222 +350,24 @@ const EncounterList = () => {
           </MyButton>
         </div>
       </Form>
-      <Table
+
+      <MyTable
         height={450}
-        sortColumn={listRequest.sortBy}
-        sortType={listRequest.sortType}
-        onSortColumn={(sortBy, sortType) => {
-          if (sortBy)
-            setListRequest({
-              ...listRequest,
-              sortBy,
-              sortType
-            });
-        }}
         data={encounterListResponse?.object ?? []}
+        columns={tableColumns}
+        rowClassName={isSelected}
+        loading={isLoading || (manualSearchTriggered && isFetching)}
         onRowClick={rowData => {
           setLocalEncounter(rowData);
           setLocalPatient(rowData.patientObject);
         }}
-        rowClassName={isSelected}
-        loading={isLoading || (manualSearchTriggered && isFetching)}
-      >
-        <Column flexGrow={1} fullText>
-          <HeaderCell>
-            <Translate>#</Translate>
-          </HeaderCell>
-          <Cell dataKey="queueNumber" />
-        </Column>
-        <Column flexGrow={6} fullText>
-          <HeaderCell fullText>
-            <Translate>PATIENT NAME</Translate>
-          </HeaderCell>
-          <Cell dataKey="patientFullName" fullText>
-            {rowData => {
-              const tooltipSpeaker = (
-                <Tooltip>
-                  <div>MRN : {rowData?.patientObject?.patientMrn}</div>
-                  <div>Age : {rowData?.patientAge}</div>
-                  <div>
-                    Gender :{' '}
-                    {rowData?.patientObject?.genderLvalue
-                      ? rowData?.patientObject?.genderLvalue?.lovDisplayVale
-                      : rowData?.patientObject?.genderLkey}
-                  </div>
-                  <div>Visit ID : {rowData?.visitId}</div>
-                </Tooltip>
-              );
-              return (
-                <Whisper trigger="hover" placement="top" speaker={tooltipSpeaker}>
-                  <div style={{ display: 'inline-block' }}>
-                    {rowData?.patientObject?.privatePatient ? (
-                      <Badge color="cyan" content="Private">
-                        <p style={{ marginTop: '5px', cursor: 'pointer' }}>
-                          {rowData?.patientObject?.fullName}
-                        </p>
-                      </Badge>
-                    ) : (
-                      <p style={{ cursor: 'pointer' }}>{rowData?.patientObject?.fullName}</p>
-                    )}
-                  </div>
-                </Whisper>
-              );
-            }}
-          </Cell>
-        </Column>
-        <Column flexGrow={4} fullText>
-          <HeaderCell>
-            <Translate>VISIT TYPE</Translate>
-          </HeaderCell>
-          <Cell>
-            {rowData =>
-              rowData.visitTypeLvalue
-                ? rowData.visitTypeLvalue.lovDisplayVale
-                : rowData.visitTypeLkey
-            }
-          </Cell>
-        </Column>
-        <Column flexGrow={4} fullText>
-          <HeaderCell>
-            <Translate> CHIEF COMPLAIN </Translate>
-          </HeaderCell>
-          <Cell>{rowData => rowData.chiefComplaint}</Cell>
-        </Column>
-        <Column flexGrow={4} fullText>
-          <HeaderCell>
-            <Translate>DIAGNOSIS</Translate>
-          </HeaderCell>
-          <Cell>{rowData => rowData.diagnosis}</Cell>
-        </Column>
-        <Column flexGrow={3} fullText>
-          <HeaderCell>
-            <Translate>PRESCRIPTION</Translate>
-          </HeaderCell>
-          <Cell>
-            {rowData =>
-              rowData.hasPrescription ? (
-                <Badge content="YES" className="status-yes" />
-              ) : (
-                <Badge className="status-no" content="NO" />
-              )
-            }
-          </Cell>
-        </Column>
-        <Column flexGrow={3}>
-          <HeaderCell>
-            <Translate>HAS ORDER</Translate>
-          </HeaderCell>
-          <Cell>
-            {rowData =>
-              rowData.hasOrder ? (
-                <Badge content="YES" className=" status-yes" />
-              ) : (
-                <Badge className="status-no" content="NO" />
-              )
-            }
-          </Cell>
-        </Column>
-        <Column flexGrow={3}>
-          <HeaderCell>
-            <Translate>PRIORITY</Translate>
-          </HeaderCell>
-          <Cell>
-            {rowData =>
-              rowData.encounterPriorityLvalue
-                ? rowData.encounterPriorityLvalue.lovDisplayVale
-                : rowData.encounterPriorityLkey
-            }
-          </Cell>
-        </Column>
-        <Column flexGrow={3}>
-          <HeaderCell>
-            <Translate>DATE</Translate>
-          </HeaderCell>
-          <Cell dataKey="plannedStartDate" />
-        </Column>
-        <Column flexGrow={3}>
-          <HeaderCell>
-            <Translate>STATUS</Translate>
-          </HeaderCell>
-          <Cell>
-            {rowData =>
-              rowData.encounterStatusLvalue
-                ? rowData.encounterStatusLvalue.lovDisplayVale
-                : rowData.encounterStatusLkey
-            }
-          </Cell>
-        </Column>
-        <Column>
-          <HeaderCell>
-            <Translate>IS OBSERVED</Translate>
-          </HeaderCell>
-          <Cell>
-            {rowData =>
-              rowData.hasObservation ? (
-                <Badge content="YES" className="status-yes" />
-              ) : (
-                <Badge className="status-no" content="NO" />
-              )
-            }
-          </Cell>
-        </Column>
-        <Column>
-          <HeaderCell>
-            <Translate> </Translate>
-          </HeaderCell>
-          <Cell style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-            {rowData => {
-              const tooltipNurse = (
-                <Tooltip>
-                  <div>Nurse Station</div>
-                </Tooltip>
-              );
-              const tooltipDoctor = (
-                <Tooltip>
-                  <div>Go to Visit</div>
-                </Tooltip>
-              );
-              return (
-                <Form layout="inline" fluid className="nurse-doctor-form">
-                  <Whisper trigger="hover" placement="top" speaker={tooltipDoctor}>
-                    <div>
-                      <MyButton
-                        size="small"
-                        onClick={() => {
-                          const encounterData = rowData;
-                          const patientData = rowData.patientObject;
-                          setLocalEncounter(encounterData);
-                          setLocalPatient(patientData);
-                          handleGoToVisit(encounterData, patientData);
-                        }}
-                      >
-                        <FontAwesomeIcon icon={faUserDoctor} />
-                      </MyButton>
-                    </div>
-                  </Whisper>
-                  <Whisper trigger="hover" placement="top" speaker={tooltipNurse}>
-                    <div>
-                      <MyButton
-                        size="small"
-                        backgroundColor="black"
-                        onClick={() => {
-                          const encounterData = rowData;
-                          const patientData = rowData.patientObject;
-                          setLocalEncounter(encounterData);
-                          setLocalPatient(patientData);
-                          handleGoToPreVisitObservations(encounterData, patientData);
-                        }}
-                      >
-                        <FontAwesomeIcon icon={faUserNurse} />
-                      </MyButton>
-                    </div>
-                  </Whisper>
-                </Form>
-              );
-            }}
-          </Cell>
-        </Column>
-      </Table>
+        sortColumn={listRequest.sortBy}
+        sortType={listRequest.sortType}
+        onSortChange={(sortBy, sortType) => {
+          setListRequest({ ...listRequest, sortBy, sortType });
+        }}
+      />
+
       <div style={{ padding: 20 }}>
         <Pagination
           prev
