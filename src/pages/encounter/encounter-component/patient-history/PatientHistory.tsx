12b4d8a0ad3ec96793ec46bf2ed121287b144f03
Batool Sbaih <148275228+BatoolSbaih@users.noreply.github.com>
2025-04-29 12:44:20 +0300

    LV

--- src/pages/encounter/encounter-component/patient-history/PatientHistory.tsx
+++ src/pages/encounter/encounter-component/patient-history/PatientHistory.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import {Tabs} from 'rsuite';
 import MedicalHistory from './MedicalHistory/MedicalHistory';
 import SurgicalHistory from './SurgicalHistory';
+import SocialHistory from './SocialHistory';
 const PatientHistory =({patient,encounter})=>{
   return(
 
     <Tabs defaultActiveKey="1" appearance="subtle" className="tab-container">
      <Tabs.Tab eventKey="1" title="Medical History"  ><MedicalHistory patient={patient} encounter={encounter}/></Tabs.Tab>
      <Tabs.Tab eventKey="2" title="Surgical History" ><SurgicalHistory patient={patient} encounter={encounter}/></Tabs.Tab>
-     <Tabs.Tab eventKey="3" title="Family History" ><SurgicalHistory patient={patient} encounter={encounter}/></Tabs.Tab>
+     <Tabs.Tab eventKey="3" title="Social History" ><SocialHistory patient={patient} encounter={encounter}/></Tabs.Tab>
     </Tabs>
 );
 };
