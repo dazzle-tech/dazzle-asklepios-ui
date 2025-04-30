9db524b78e0c549d8b0aaa522a673c648bcb9c14
Batool Sbaih <148275228+BatoolSbaih@users.noreply.github.com>
2025-04-30 11:18:35 +0300

    AS-1121

--- src/pages/setup/cpt-setup/CPTSetup.tsx
+++ src/pages/setup/cpt-setup/CPTSetup.tsx
@@ -1,21 +1,18 @@
-import Translate from '@/components/Translate';
 import { initialListRequest, ListRequest } from '@/types/types';
-import React, { useState ,useEffect } from 'react';
-
-import { Input, Modal, Pagination, Panel, Table } from 'rsuite';
-const { Column, HeaderCell, Cell } = Table;
+import React, { useState, useEffect } from 'react';
+import MyTable from '@/components/MyTable';
+import { Form } from 'rsuite';
 import { addFilterToListRequest, fromCamelCaseToDBName } from '@/utils';
-import { BlockUI } from 'primereact/blockui';
-import {
-    useGetCptListQuery,
-} from '@/services/setupService';
-import { useSelector } from 'react-redux';
-import { RootState } from '@/store';
+import { useGetCptListQuery } from '@/services/setupService';
+import MyInput from '@/components/MyInput';
 import ReactDOMServer from 'react-dom/server';
 import { setDivContent, setPageCode } from '@/reducers/divSlice';
 import { useAppDispatch } from '@/hooks';
+
 const CPTSetup = () => {
     const dispatch = useAppDispatch();
+
+    // Initial table request with default filter (excluding deleted records)
     const [listRequest, setListRequest] = useState<ListRequest>({
         ...initialListRequest,
         filters: [
@@ -25,21 +22,28 @@ const CPTSetup = () => {
                 value: undefined
             }
         ],
-        pageSize: 15
     });
+
+    // State to handle the filter form inputs
+    const [record, setRecord] = useState({ filter: '', value: '' });
+
+    // Fetch the CPT list data based on current filters
     const { data: cptListResponseLoading } = useGetCptListQuery(listRequest);
-    console.log(cptListResponseLoading?.object)
-        const divElement = useSelector((state: RootState) => state.div?.divElement);
-        const divContent = (
-          <div style={{ display: 'flex' }}>
+
+    // Header page setUp
+    const divContent = (
+        <div style={{ display: 'flex' }}>
             <h5>CPT Diagnosis List</h5>
-          </div>
-        );
-        const divContentHTML = ReactDOMServer.renderToStaticMarkup(divContent);
-        dispatch(setPageCode('CPT'));
-        dispatch(setDivContent(divContentHTML));
+        </div>
+    );
+    const divContentHTML = ReactDOMServer.renderToStaticMarkup(divContent);
+    dispatch(setPageCode('CPT')); // Set page code in Redux
+    dispatch(setDivContent(divContentHTML)); // Set header content in Redux
+
+    // Handle changes in filter fields
     const handleFilterChange = (fieldName, value) => {
         if (value) {
+            // Add new filter to the list request
             setListRequest(
                 addFilterToListRequest(
                     fromCamelCaseToDBName(fieldName),
@@ -49,6 +53,7 @@ const CPTSetup = () => {
                 )
             );
         } else {
+            // Reset to default filter if value is empty
             setListRequest({
                 ...listRequest, filters: [
                     {
@@ -57,97 +62,117 @@ const CPTSetup = () => {
                         value: undefined
                     }
                 ],
-                pageSize: 15
             });
         }
     };
+
+    // Change page event handler
+    const handlePageChange = (_: unknown, newPage: number) => {
+        setListRequest({ ...listRequest, pageNumber: newPage + 1 });
+    };
+    // Change number of rows per page
+    const handleRowsPerPageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setListRequest({
+            ...listRequest,
+            pageSize: parseInt(event.target.value, 10),
+            pageNumber: 1 // Reset to first page
+        });
+    };
+    // Table columns definition
+    const columns = [
+        {
+            key: 'cptCode',
+            title: 'Code',
+            render: (rowData) => rowData?.cptCode ?? 'N/A',
+        },
+        {
+            key: 'category',
+            title: 'Category',
+            render: (rowData) =>
+                rowData.categoryLvalue
+                    ? rowData.categoryLvalue.lovDisplayVale
+                    : rowData.categoryLkey,
+        },
+        {
+            key: 'description',
+            title: 'Description',
+            render: (rowData) => rowData?.description ?? 'N/A',
+        }
+    ];
+
+    // Available fields for filtering
+    const filterFields = [
+        { label: 'Code', value: 'cptCode' },
+        { label: 'Category', value: 'category' },
+        { label: 'Description', value: 'description' }
+    ];
+
+    // Filter form rendered above the table
+    const filters = () => (
+        <Form layout="inline" fluid>
+            <MyInput
+                selectDataValue="value"
+                selectDataLabel="label"
+                selectData={filterFields}
+                fieldName="filter"
+                fieldType="select"
+                record={record}
+                setRecord={(updatedRecord) => {
+                    setRecord({
+                        ...record,
+                        filter: updatedRecord.filter,
+                        value: '' // Clear the text input whenever filter is changed
+                    });
+                }}
+                showLabel={false}
+                placeholder="Select Filter"
+                searchable={false}
+            />
+
+            <MyInput
+                fieldName="value"
+                fieldType="text"
+                record={record}
+                setRecord={setRecord}
+                showLabel={false}
+                placeholder="Search"
+            />
+        </Form>
+    );
+
+    // Pagination values
+    const pageIndex = listRequest.pageNumber - 1;
+    const rowsPerPage = listRequest.pageSize;
+    const totalCount = cptListResponseLoading?.extraNumeric ?? 0;
+
+
+    // Trigger filter logic when filter form changes
+    useEffect(() => {
+        if (record['filter']) {
+            handleFilterChange(record['filter'], record['value']);
+        }
+    }, [record]);
+
+    // Clear page info on component unmount
     useEffect(() => {
         return () => {
-          dispatch(setPageCode(''));
-          dispatch(setDivContent("  "));
+            dispatch(setPageCode(''));
+            dispatch(setDivContent("  "));
         };
-      }, [location.pathname, dispatch])
-    return (<>
-        <Panel
-        >
-            <Table
-                height={400}
-                sortColumn={listRequest.sortBy}
-                sortType={listRequest.sortType}
-                onSortColumn={(sortBy, sortType) => {
-                    if (sortBy)
-                        setListRequest({
-                            ...listRequest,
-                            sortBy,
-                            sortType
-                        });
-                }}
-                headerHeight={80}
-                rowHeight={60}
-                bordered
-                cellBordered
-                data={cptListResponseLoading?.object ?? []}>
-                <Table.Column sortable flexGrow={2}>
-                    <Table.HeaderCell align="center">
-                        <Input onChange={value => handleFilterChange('cptCode', value)} />
-                        <Translate>Code</Translate>
-                    </Table.HeaderCell>
-                    <Table.Cell>
-                        {rowData => rowData?.cptCode ?? 'N/A'}
-                    </Table.Cell>
-                </Table.Column>
+    }, [location.pathname, dispatch]);
 
-                <Table.Column sortable flexGrow={2}>
-                    <Table.HeaderCell align="center">
-                        <Input onChange={value => handleFilterChange('categoryLvalue.lovDisplayVale', value)} />
-                        <Translate>Category</Translate>
-                    </Table.HeaderCell>
-                    <Table.Cell>
-                        
-                                {rowData =>
-                          rowData.categoryLvalue
-                            ? rowData.categoryLvalue.lovDisplayVale
-                            : rowData.categoryLkey
-                        }
-                    </Table.Cell>
-                </Table.Column>
-
-                <Table.Column sortable flexGrow={2}>
-                    <Table.HeaderCell align="center">
-                        <Input onChange={value => handleFilterChange('description', value)} />
-                        <Translate>Description</Translate>
-                    </Table.HeaderCell>
-                    <Table.Cell>
-                        {rowData => rowData?.description ?? 'N/A'}
-                    </Table.Cell>
-                </Table.Column>
-            </Table>
-
-            <div style={{ padding: 20 }}>
-                <Pagination
-                    prev
-                    next
-                    first
-                    last
-                    ellipsis
-                    boundaryLinks
-                    maxButtons={5}
-                    size="xs"
-                    layout={['limit', '|', 'pager']}
-                    limitOptions={[5, 15, 30]}
-                    limit={listRequest.pageSize}
-                    activePage={listRequest.pageNumber}
-                    onChangePage={pageNumber => {
-                        setListRequest({ ...listRequest, pageNumber });
-                    }}
-                    onChangeLimit={pageSize => {
-                        setListRequest({ ...listRequest, pageSize });
-                    }}
-                    total={cptListResponseLoading?.extraNumeric ?? 0}
-                />
-            </div>
-
-        </Panel>
-    </>)
+    return (
+        <MyTable
+            data={cptListResponseLoading?.object ?? []}
+            columns={columns}
+            filters={filters()}
+            page={pageIndex}
+            rowsPerPage={rowsPerPage}
+            totalCount={totalCount}
+            onPageChange={handlePageChange}
+            onRowsPerPageChange={handleRowsPerPageChange}
+        />
+    )
 }
+
 export default CPTSetup;
