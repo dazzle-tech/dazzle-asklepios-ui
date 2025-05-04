7f2fe7791d8b2ebcf7aaa2fc02b8694007863b32
Rawan-Yahya200 <rawanyahya300@gmail.com>
2025-05-04 09:18:48 +0300

    AS-911

--- src/pages/setup/departments-setup/ChooseScreen.tsx
+++ src/pages/setup/departments-setup/ChooseScreen.tsx
@@ -1,26 +1,26 @@
 import MyModal from '@/components/MyModal/MyModal';
 import React from 'react';
-import { faLaptop } from '@fortawesome/free-solid-svg-icons';
 import { useSaveMedicalSheetMutation } from '@/services/setupService';
 import MyInput from '@/components/MyInput';
 import { Form } from 'rsuite';
 import { useDispatch } from 'react-redux';
 import { notify } from '@/utils/uiReducerActions';
-import { Col, Row, Panel } from 'rsuite';
 import { faSheetPlastic } from '@fortawesome/free-solid-svg-icons';
+import './styles.less';
 
 const ChooseDepartment = ({
   open,
   setOpen,
-  //   width,
+  width,
   showScreen,
   setShowScreen,
   department
-  //   refetch,
 }) => {
   const dispatch = useDispatch();
+  // Save MedicalSheet
   const [saveMedicalSheet] = useSaveMedicalSheetMutation();
 
+  // Handle save MedicalSheet
   const handleSave = () => {
     try {
       saveMedicalSheet({ ...showScreen, departmentKey: department.key }).unwrap();
@@ -30,30 +30,14 @@ const ChooseDepartment = ({
       dispatch(notify({ msg: 'Saved Faild', sev: 'error' }));
     }
   };
+  // Modal content
   const conjureFormContent = (stepNumber = 0) => {
     switch (stepNumber) {
       case 0:
         return (
           <Form fluid>
-            <div
-              style={{
-                display: 'flex',
-                gap: '10px',
-                justifyContent: 'center',
-                alignContent: 'center',
-                alignItems: 'center'
-              }}
-            >
-              <div
-                style={{
-                  width: 300,
-                  height: 300,
-                  backgroundColor: 'var(--background-gray)',
-                  borderRadius: '10px',
-                  overflow: 'auto',
-                  padding: '10px'
-                }}
-              >
+            <div className="container-of-medical-sheets">
+              <div className="container-of-specific-sheets">
                 <h6>Default Sheets</h6>
                 <Form>
                   <MyInput
@@ -65,7 +49,7 @@ const ChooseDepartment = ({
                     setRecord={() => ''}
                   />
                 </Form>
-                <div style={{ marginLeft: '20px' }}>
+                <div className="sheets">
                   <Form>
                     <MyInput
                       fieldType="check"
@@ -194,16 +178,7 @@ const ChooseDepartment = ({
                   </Form>
                 </div>
               </div>
-              <div
-                style={{
-                  width: 300,
-                  height: 300,
-                  backgroundColor: 'var(--background-gray)',
-                  borderRadius: '10px',
-                  overflow: 'auto',
-                  padding: '10px'
-                }}
-              >
+              <div className="container-of-specific-sheets">
                 <h6>Specialty Sheets</h6>
                 <Form>
                   <MyInput
@@ -215,7 +190,7 @@ const ChooseDepartment = ({
                     setRecord={() => ''}
                   />
                 </Form>
-                <div style={{ marginLeft: '20px' }}>
+                <div className="sheets">
                   <Form>
                     <MyInput
                       fieldType="check"
@@ -277,17 +252,16 @@ const ChooseDepartment = ({
         );
     }
   };
-
   return (
     <MyModal
       open={open}
       setOpen={setOpen}
-      title={'Medical Sheets'}
+      title='Medical Sheets'
       position="right"
       content={conjureFormContent}
-      actionButtonLabel={department?.key ? 'Save' : 'Create'}
+      actionButtonLabel='Save'
       actionButtonFunction={handleSave}
-      size="600px"
+      size={width > 600 ? '570px' : '300px'}
       steps={[{ title: 'Medical Sheets', icon: faSheetPlastic }]}
     />
   );
