66d9da79045c12fac28203241eba7c63f241a370
hanan253yahya <165596621+hanan253yahya@users.noreply.github.com>
2025-05-19 13:12:03 +0300

    as-1483

--- src/pages/encounter/encounter-component/patient-summary/PreviuosVisitData/PreviuosVisitData.tsx
+++ src/pages/encounter/encounter-component/patient-summary/PreviuosVisitData/PreviuosVisitData.tsx
@@ -1,5 +1,5 @@
-import React, { useState ,useEffect } from 'react';
-import { Divider ,Form  } from 'rsuite';
+import React, { useState, useEffect } from 'react';
+import { Divider, Form } from 'rsuite';
 import MyInput from '@/components/MyInput';
 import { ApEncounter } from '@/types/model-types';
 import { newApEncounter } from '@/types/model-types-constructor';
@@ -7,16 +7,16 @@ import { useGetLovValuesByCodeQuery } from '@/services/setupService';
 import { useGetPatientDiagnosisQuery } from '@/services/encounterService';
 import { useGetEncountersQuery } from '@/services/encounterService';
 import { initialListRequest, ListRequest } from '@/types/types';
-const PreviuosVisitData = ({ patient ,encounter }) => {
-      const [prevencounter, setPrevencounter] = useState<ApEncounter>({ ...newApEncounter });
+const PreviuosVisitData = ({ patient, encounter }) => {
+    const [prevencounter, setPrevencounter] = useState<ApEncounter>({ ...newApEncounter });
 
-      const { data: encounterTypeLovQueryResponse } = useGetLovValuesByCodeQuery('BOOK_VISIT_TYPE');
-      const { data: encounterReasonLovQueryResponse } = useGetLovValuesByCodeQuery('ENC_REASON');
-      
+    const { data: encounterTypeLovQueryResponse } = useGetLovValuesByCodeQuery('BOOK_VISIT_TYPE');
+    const { data: encounterReasonLovQueryResponse } = useGetLovValuesByCodeQuery('ENC_REASON');
     const [patientVisitListRequest, setPatientVisitListReques] = useState<ListRequest>({
         ...initialListRequest,
 
         sortBy: 'plannedStartDate',
+        sortType:"desc",
         filters: [
             {
 
@@ -33,72 +33,73 @@ const PreviuosVisitData = ({ patient ,encounter }) => {
         ],
 
     });
-     const [listdRequest, setListdRequest] = useState({
-          ...initialListRequest,
-          pageSize: 100,
-          timestamp: new Date().getMilliseconds(),
-          sortBy: 'createdAt',
-          sortType: 'desc',
-          filters: [
-              {
-                  fieldName: 'patient_key',
-                  operator: 'match',
-                  value: patient?.key
-              },
-              {
-                  fieldName: 'visit_key',
-                  operator: 'match',
-                  value: prevencounter?.key
-              }
-  
-  
-          ]
-      });
-          const { data: Diagnoses, refetch: fetchlastDiag } = useGetPatientDiagnosisQuery(listdRequest
-              , {
-                  refetchOnMountOrArgChange: true,
-                  refetchOnFocus: true
-              }
-      
-          )
-              const { data: encounterPatientList } = useGetEncountersQuery({ ...patientVisitListRequest }, {
-                  refetchOnMountOrArgChange: true,
-                  refetchOnFocus: true,
-              });
-           useEffect(() => {
-                  setPrevencounter(encounterPatientList?.object[0]);
-              }, [encounterPatientList])
-   useEffect(() => {
-          const updatedFilters = [
-              {
-                  fieldName: 'patient_key',
-                  operator: 'match',
-                  value: patient?.key
-              },
-              {
-                  fieldName: 'visit_key',
-                  operator: 'match',
-                  value: prevencounter?.key
-              }
-          ];
-          setListdRequest((prevRequest) => ({
-  
-              ...prevRequest,
-              filters: updatedFilters,
-  
-          }));
-      }, [prevencounter]);
+    const [listdRequest, setListdRequest] = useState({
+        ...initialListRequest,
+        pageSize: 100,
+        timestamp: new Date().getMilliseconds(),
+        sortBy: 'createdAt',
+        sortType: 'desc',
+        filters: [
+            {
+                fieldName: 'patient_key',
+                operator: 'match',
+                value: patient?.key
+            },
+            {
+                fieldName: 'visit_key',
+                operator: 'match',
+                value: prevencounter?.key
+            }
+
+
+        ]
+    });
+    const { data: Diagnoses, refetch: fetchlastDiag } = useGetPatientDiagnosisQuery(listdRequest
+        , {
+            refetchOnMountOrArgChange: true,
+            refetchOnFocus: true
+        }
+
+    )
+    const { data: encounterPatientList } = useGetEncountersQuery({ ...patientVisitListRequest }, {
+        refetchOnMountOrArgChange: true,
+        refetchOnFocus: true,
+    });
+
+    useEffect(() => {
+        setPrevencounter(encounterPatientList?.object[0]);
+    }, [encounterPatientList])
+    useEffect(() => {
+        const updatedFilters = [
+            {
+                fieldName: 'patient_key',
+                operator: 'match',
+                value: patient?.key
+            },
+            {
+                fieldName: 'visit_key',
+                operator: 'match',
+                value: prevencounter?.key
+            }
+        ];
+        setListdRequest((prevRequest) => ({
+
+            ...prevRequest,
+            filters: updatedFilters,
+
+        }));
+    }, [prevencounter]);
     return (
         <div className='medical-dashboard-main-container'>
             <div className='medical-dashboard-container-div'>
                 <div className='medical-dashboard-header-div'>
                     <div className='medical-dashboard-title-div'>
-                      Previuos Visit
+                        Previuos Visit
                     </div>
                 </div>
                 <Divider className="divider-line" />
                 <div className='medical-dashboard-content-div'>
-                <Form disabled layout="inline" fluid >
+                    <Form disabled layout="inline" fluid >
                         <MyInput
                             column
                             width={140}
@@ -137,11 +138,11 @@ const PreviuosVisitData = ({ patient ,encounter }) => {
                             fieldName="description"
                             record={Diagnoses?.object[0]?.diagnosisObject || {}}
                         />
-                        </Form>
-   
+                    </Form>
+
                 </div>
             </div>
-            
+
         </div>
     );
 };
