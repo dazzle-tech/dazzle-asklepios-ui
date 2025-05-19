f4b0e80d472b3b6b372ded11ad9d7c998dd747ce
Batool Sbaih <148275228+BatoolSbaih@users.noreply.github.com>
2025-05-19 11:41:25 +0300

    AS-1076

--- src/pages/encounter/encounter-pre-observations/previous-measurements/PreviousMeasurements.tsx
+++ src/pages/encounter/encounter-pre-observations/previous-measurements/PreviousMeasurements.tsx
@@ -3,69 +3,44 @@ import './styles.less';
 import MyTable from '@/components/MyTable';
 import MyInput from '@/components/MyInput';
 import { Form } from 'rsuite';
-import MyButton from '@/components/MyButton/MyButton';
-import * as icons from '@rsuite/icons';
-const PreviousMeasurements = ({patient }) => {
-      const filters = () => {
-        return (
-          <Form layout="inline" fluid>
-            <MyInput
-              column
-              width={180}
-              fieldType="date"
-              fieldLabel="Date"
-              fieldName=""
-              record={""}
-              setRecord={"setDateFilter"}
-            />
-              <MyButton>
-                <icons.Search />
-              </MyButton>
-          </Form>
-        );
-      };
-    // Table Column
-    const columns = [
-        {
-            key: '',
-            title: 'Date',
-        },
-        {
-            key: '',
-            title: 'weight',
-        },
-        {
-            key: '',
-            title: 'height',  
-        },
-        {
-            key: '',
-            title: 'temperature',
-        },
-        {
-            key: '',
-            title: 'pulse rate',
-            dataKey: '',
-        },
-        {
-            key: '',
-            title: 'blood pressure',
-        },
-        {
-            key: '',
-            title: 'blood glucose',
-        },
-          {
-            key: '',
-            title: 'oxygen saturation',
-        }
-    ];
-    return (<div>
-        <MyTable
-            data={[]}
-            columns={columns}
-            filters={()=>filters()}
-        />
-    </div>);
+const PreviousMeasurements = ({ patient }) => {
+  const [dateFilter, setDateFilter] = React.useState('');
+
+  const filters = () => {
+    return (
+       <Form layout="inline" fluid >
+      <MyInput
+        column
+        width={180}
+        fieldType="date"
+        fieldLabel="Date"
+        fieldName="date"
+        record={dateFilter}
+        setRecord={setDateFilter}
+      />
+    </Form>
+    );
+  };
+
+  const columns = [
+    { key: 'date', title: 'Date', dataKey: 'date' },
+    { key: 'weight', title: 'Weight', dataKey: 'weight' },
+    { key: 'height', title: 'Height', dataKey: 'height' },
+    { key: 'temperature', title: 'Temperature', dataKey: 'temperature' },
+    { key: 'pulseRate', title: 'Pulse Rate', dataKey: 'pulseRate' },
+    { key: 'bloodPressure', title: 'Blood Pressure', dataKey: 'bloodPressure' },
+    { key: 'bloodGlucose', title: 'Blood Glucose', dataKey: 'bloodGlucose' },
+    { key: 'oxygenSaturation', title: 'Oxygen Saturation', dataKey: 'oxygenSaturation' },
+  ];
+
+  return (
+    <div>
+      <MyTable
+        data={[]}
+        columns={columns}
+        filters={filters()} />
+    </div>
+  );
 };
+
 export default PreviousMeasurements;
