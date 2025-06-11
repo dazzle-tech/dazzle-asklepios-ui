95cbebc8a012ddf42b223edc5c646cb13f59d42e
Rawan-Yahya200 <rawanyahya300@gmail.com>
2025-06-11 09:08:42 +0300

    features/AS-1174

--- src/pages/setup/allergens-setup/Allergens.tsx
+++ src/pages/setup/allergens-setup/Allergens.tsx
@@ -1,38 +1,53 @@
 import Translate from '@/components/Translate';
 import { initialListRequest, ListRequest } from '@/types/types';
 import React, { useState, useEffect } from 'react';
-import { Input, Modal, Pagination, Panel, Table } from 'rsuite';
-const { Column, HeaderCell, Cell } = Table;
-import {
-  useGetAllergensQuery,
-  useSaveAllergensMutation,
-  useGetLovValuesByCodeQuery
-} from '@/services/setupService';
-import { Button, ButtonToolbar, IconButton } from 'rsuite';
+import { Panel } from 'rsuite';
+import { useGetAllergensQuery, useSaveAllergensMutation } from '@/services/setupService';
 import AddOutlineIcon from '@rsuite/icons/AddOutline';
-import EditIcon from '@rsuite/icons/Edit';
-import TrashIcon from '@rsuite/icons/Trash';
 import { ApAllergens } from '@/types/model-types';
 import { newApAllergens } from '@/types/model-types-constructor';
-import { Form, Stack, Divider } from 'rsuite';
+import { Form } from 'rsuite';
 import MyInput from '@/components/MyInput';
 import { addFilterToListRequest, fromCamelCaseToDBName } from '@/utils';
-import { useSelector } from 'react-redux';
-import { RootState } from '@/store';
 import ReactDOMServer from 'react-dom/server';
 import { setDivContent, setPageCode } from '@/reducers/divSlice';
 import { useAppDispatch } from '@/hooks';
+import { MdModeEdit } from 'react-icons/md';
+import { FaUndo } from 'react-icons/fa';
+import { MdDelete } from 'react-icons/md';
+import MyTable from '@/components/MyTable';
+import AddEditAllergens from './AddEditAllergens';
+import MyButton from '@/components/MyButton/MyButton';
+import DeletionConfirmationModal from '@/components/DeletionConfirmationModal';
+import './styles.less';
+import { notify } from '@/utils/uiReducerActions';
 const Allergens = () => {
   const dispatch = useAppDispatch();
   const [allergens, setAllergens] = useState<ApAllergens>({ ...newApAllergens });
+  const [width, setWidth] = useState<number>(window.innerWidth);
   const [popupOpen, setPopupOpen] = useState(false);
+  const [openConfirmDeleteAllergens, setOpenConfirmDeleteAllergens] = useState<boolean>(false);
+  const [stateOfDeleteAllergens, setStateOfDeleteAllergens] = useState<string>('delete');
+  const [recordOfFilter, setRecordOfFilter] = useState({ filter: '', value: '' });
   const [listRequest, setListRequest] = useState<ListRequest>({ ...initialListRequest });
+  // Fetch allergens list response
+  const { data: allergensListResponse, isFetching } = useGetAllergensQuery(listRequest);
+  // Save Allergens
   const [saveAllergens, saveAllergensMutation] = useSaveAllergensMutation();
-  const { data: allergensListResponse } = useGetAllergensQuery(listRequest);
-  const { data: allergensTypeLovQueryResponse } = useGetLovValuesByCodeQuery('ALLERGEN_TYPES');
-  const divElement = useSelector((state: RootState) => state.div?.divElement);
+  // Pagination values
+  const pageIndex = listRequest.pageNumber - 1;
+  const rowsPerPage = listRequest.pageSize;
+  const totalCount = allergensListResponse?.extraNumeric ?? 0;
+  // Available fields for filtering
+  const filterFields = [
+    { label: 'Allergens Code', value: 'allergenCode' },
+    { label: 'Allergens Name', value: 'allergenName' },
+    { label: 'Allergens Type', value: 'allergenTypeLkey' },
+    { label: 'Description', value: 'description' }
+  ];
+  // Header page setUp
   const divContent = (
-    <div style={{ display: 'flex' }}>
+    <div className="title-allergens">
       <h5>Allergens</h5>
     </div>
   );
@@ -40,28 +55,115 @@ const Allergens = () => {
   dispatch(setPageCode('Allergens'));
   dispatch(setDivContent(divContentHTML));
 
-  const handleNew = () => {
-    setAllergens({...newApAllergens})
-    setPopupOpen(true);
-  };
-
-  const handleSave = () => {
-    setPopupOpen(false);
-    saveAllergens(allergens).unwrap();
-  };
-
-  useEffect(() => {
-    if (saveAllergensMutation.data) {
-      setListRequest({ ...listRequest, timestamp: new Date().getTime() });
-    }
-  }, [saveAllergensMutation.data]);
-
+  // class name for selected row
   const isSelected = rowData => {
     if (rowData && allergens && rowData.key === allergens.key) {
       return 'selected-row';
     } else return '';
   };
 
+  // Filter table
+  const filters = () => (
+    <Form layout="inline" fluid className="container-of-filter-fields-allergens">
+      <MyInput
+        selectDataValue="value"
+        selectDataLabel="label"
+        selectData={filterFields}
+        fieldName="filter"
+        fieldType="select"
+        record={recordOfFilter}
+        setRecord={updatedRecord => {
+          setRecordOfFilter({
+            ...recordOfFilter,
+            filter: updatedRecord.filter,
+            value: ''
+          });
+        }}
+        showLabel={false}
+        placeholder="Select Filter"
+        searchable={false}
+      />
+      <MyInput
+        fieldName="value"
+        fieldType="text"
+        record={recordOfFilter}
+        setRecord={setRecordOfFilter}
+        showLabel={false}
+        placeholder="Search"
+      />
+    </Form>
+  );
+
+  // Icons column ( Edit, reactive/Deactivate)
+  const iconsForActions = (rowData: ApAllergens) => (
+    <div className="container-of-icons-allergens">
+      <MdModeEdit
+        className="icons-allergens"
+        title="Edit"
+        size={24}
+        fill="var(--primary-gray)"
+        onClick={() => setPopupOpen(true)}
+      />
+      {rowData?.isValid ? (
+        <MdDelete
+          className="icons-allergens"
+          title="Deactivate"
+          size={24}
+          fill="var(--primary-pink)"
+          onClick={() => {
+            setStateOfDeleteAllergens('deactivate');
+            setOpenConfirmDeleteAllergens(true);
+          }}
+        />
+      ) : (
+        <FaUndo
+          className="icons-allergens"
+          title="Activate"
+          size={20}
+          fill="var(--primary-gray)"
+          onClick={() => {
+            setStateOfDeleteAllergens('reactivate');
+            setOpenConfirmDeleteAllergens(true);
+          }}
+        />
+      )}
+    </div>
+  );
+
+  //Table columns
+  const tableColumns = [
+    {
+      key: 'allergenCode',
+      title: <Translate>Allergens Code</Translate>,
+      flexGrow: 4
+    },
+    {
+      key: 'allergenName',
+      title: <Translate>Allergens Name</Translate>,
+      flexGrow: 4
+    },
+    {
+      key: 'allergenTypeLkey',
+      title: <Translate>Allergens Type</Translate>,
+      flexGrow: 4,
+      render: rowData =>
+        rowData.allergenTypeLvalue
+          ? rowData.allergenTypeLvalue.lovDisplayVale
+          : rowData.allergenTypeLkey
+    },
+    {
+      key: 'description',
+      title: <Translate>Description</Translate>,
+      flexGrow: 4
+    },
+    {
+      key: 'icons',
+      title: <Translate></Translate>,
+      flexGrow: 3,
+      render: rowData => iconsForActions(rowData)
+    }
+  ];
+  // handle filter change
   const handleFilterChange = (fieldName, value) => {
     if (value) {
       setListRequest(
@@ -76,148 +178,168 @@ const Allergens = () => {
       setListRequest({ ...listRequest, filters: [] });
     }
   };
+
+  // handle click on add new button
+  const handleNew = () => {
+    setAllergens({ ...newApAllergens });
+    setPopupOpen(true);
+  };
+
+  // Handle page change in navigation
+  const handlePageChange = (_: unknown, newPage: number) => {
+    setListRequest({ ...listRequest, pageNumber: newPage + 1 });
+  };
+
+  // Handle change rows per page in navigation
+  const handleRowsPerPageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setListRequest({
+      ...listRequest,
+      pageSize: parseInt(event.target.value, 10),
+      pageNumber: 1
+    });
+  };
+
+  // handle save allergens
+  const handleSave = () => {
+    setPopupOpen(false);
+    saveAllergens(allergens)
+      .unwrap()
+      .then(() => {
+        dispatch(notify({ msg: 'The Allergens has been saved successfully', sev: 'success' }));
+      })
+      .catch(() => {
+        dispatch(notify({ msg: 'Failed to save this Allergens', sev: 'error' }));
+      });
+  };
+  // handle deactivate allergens
+  const handleDeactivate = () => {
+    setOpenConfirmDeleteAllergens(false);
+    saveAllergens({ ...allergens, isValid: false })
+      .unwrap()
+      .then(() => {
+        dispatch(
+          notify({
+            msg: 'The Allergens was successfully Deactivated',
+            sev: 'success'
+          })
+        );
+      })
+      .catch(() => {
+        dispatch(
+          notify({
+            msg: 'Faild to Deactivate this Allergens',
+            sev: 'error'
+          })
+        );
+      });
+  };
+  // handle reactivate allergens
+  const handleReactivate = () => {
+    setOpenConfirmDeleteAllergens(false);
+    saveAllergens({ ...allergens, isValid: true })
+      .unwrap()
+      .then(() => {
+        dispatch(
+          notify({
+            msg: 'The Allergens was successfully Reactivated',
+            sev: 'success'
+          })
+        );
+      })
+      .catch(() => {
+        dispatch(
+          notify({
+            msg: 'Faild to Reactivate this Allergens',
+            sev: 'error'
+          })
+        );
+      });
+  };
+
+  // Effects
+  // change the width variable when the size of window is changed
+  useEffect(() => {
+    const handleResize = () => setWidth(window.innerWidth);
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+  // update list when allergens is saved
+  useEffect(() => {
+    if (saveAllergensMutation.data) {
+      setListRequest({ ...listRequest, timestamp: new Date().getTime() });
+    }
+  }, [saveAllergensMutation.data]);
+
   useEffect(() => {
     return () => {
       dispatch(setPageCode(''));
-      dispatch(setDivContent("  "));
+      dispatch(setDivContent('  '));
     };
   }, [location.pathname, dispatch]);
+
+  // update the list when the filter is changed
+  useEffect(() => {
+    if (recordOfFilter['filter']) {
+      handleFilterChange(recordOfFilter['filter'], recordOfFilter['value']);
+    } else {
+      setListRequest({
+        ...initialListRequest,
+        pageSize: listRequest.pageSize,
+        pageNumber: 1
+      });
+    }
+  }, [recordOfFilter]);
+
   return (
     <Panel>
-      <ButtonToolbar>
-        <IconButton appearance="primary" icon={<AddOutlineIcon />} onClick={handleNew}>
+      <div className="container-of-add-new-button-allergens">
+        <MyButton
+          prefixIcon={() => <AddOutlineIcon />}
+          color="var(--deep-blue)"
+          onClick={handleNew}
+          width="109px"
+        >
           Add New
-        </IconButton>
-        <IconButton
-          disabled={!allergens.key}
-          appearance="primary"
-          onClick={() => setPopupOpen(true)}
-          color="green"
-          icon={<EditIcon />}
-        >
-          Edit Selected
-        </IconButton>
-        <IconButton
-          disabled={true || !allergens.key}
-          appearance="primary"
-          color="red"
-          icon={<TrashIcon />}
-        >
-          Delete Selected
-        </IconButton>
-      </ButtonToolbar>
-      <hr />
-      <Table
-        height={400}
-        sortColumn={listRequest.sortBy}
-        sortType={listRequest.sortType}
-        onSortColumn={(sortBy, sortType) => {
-          if (sortBy)
-            setListRequest({
-              ...listRequest,
-              sortBy,
-              sortType
-            });
-        }}
-        headerHeight={80}
-        rowHeight={60}
-        bordered
-        cellBordered
+        </MyButton>
+      </div>
+      <MyTable
+        height={450}
         data={allergensListResponse?.object ?? []}
+        loading={isFetching}
+        columns={tableColumns}
+        rowClassName={isSelected}
+        filters={filters()}
         onRowClick={rowData => {
           setAllergens(rowData);
         }}
-        rowClassName={isSelected}
-      >
-        <Column sortable flexGrow={2}>
-          <HeaderCell align="center">
-            <Input onChange={e => handleFilterChange('allergenCode', e)} />
-            <Translate>Allergens Code</Translate>
-          </HeaderCell>
-          <Cell dataKey="allergenCode" />
-        </Column>
-        <Column sortable flexGrow={2}>
-          <HeaderCell  align="center">
-            <Input onChange={e => handleFilterChange('allergenName', e)} />
-            <Translate>Allergens Name</Translate>
-          </HeaderCell>
-          <Cell dataKey="allergenName" />
-        </Column> 
-        <Column sortable flexGrow={2}>
-          <HeaderCell  align="center">
-            <Input onChange={e => handleFilterChange('allergenTypeLkey', e)} />
-            <Translate>Allergens Type</Translate>
-          </HeaderCell>
-          <Cell>
-            {rowData =>
-              rowData.allergenTypeLvalue ? rowData.allergenTypeLvalue.lovDisplayVale : rowData.allergenTypeLkey
-            }
-          </Cell>
-        </Column>
-        <Column sortable flexGrow={3}>
-          <HeaderCell  align="center">
-            <Input onChange={e => handleFilterChange('description', e)} />
-            <Translate>description</Translate>
-          </HeaderCell>
-          <Cell dataKey="description" />
-        </Column> 
-        
-      </Table>
-      <div style={{ padding: 20 }}>
-        <Pagination
-          prev
-          next
-          first
-          last
-          ellipsis
-          boundaryLinks
-          maxButtons={5}
-          size="xs"
-          layout={['limit', '|', 'pager']}
-          limitOptions={[5, 15, 30]}
-          limit={listRequest.pageSize}
-          activePage={listRequest.pageNumber}
-          onChangePage={pageNumber => {
-            setListRequest({ ...listRequest, pageNumber });
-          }}
-          onChangeLimit={pageSize => {
-            setListRequest({ ...listRequest, pageSize });
-          }}
-          total={allergensListResponse?.extraNumeric ?? 0}
-        />
-      </div>
-
-      <Modal open={popupOpen} overflow>
-        <Modal.Title>
-          <Translate>New/Edit Allergens</Translate>
-        </Modal.Title>
-        <Modal.Body>
-          <Form fluid>
-            <MyInput fieldName="allergenCode" record={allergens} setRecord={setAllergens} />
-            <MyInput fieldName="allergenName" record={allergens} setRecord={setAllergens} />
-            <MyInput
-              fieldName="allergenTypeLkey"
-              fieldType="select"
-              selectData={allergensTypeLovQueryResponse?.object ?? []}
-              selectDataLabel="lovDisplayVale"
-              selectDataValue="key"
-              record={allergens}
-              setRecord={setAllergens}
-            />
-             <MyInput fieldName="description" record={allergens} setRecord={setAllergens} />
-          </Form>
-        </Modal.Body>
-        <Modal.Footer>
-          <Stack spacing={2} divider={<Divider vertical />}>
-            <Button appearance="primary" onClick={handleSave}>
-              Save
-            </Button>
-            <Button appearance="primary" color="red" onClick={() => setPopupOpen(false)}>
-              Cancel
-            </Button>
-          </Stack>
-        </Modal.Footer>
-      </Modal>
+        sortColumn={listRequest.sortBy}
+        sortType={listRequest.sortType}
+        onSortChange={(sortBy, sortType) => {
+          if (sortBy) setListRequest({ ...listRequest, sortBy, sortType });
+        }}
+        page={pageIndex}
+        rowsPerPage={rowsPerPage}
+        totalCount={totalCount}
+        onPageChange={handlePageChange}
+        onRowsPerPageChange={handleRowsPerPageChange}
+      />
+      <AddEditAllergens
+        open={popupOpen}
+        setOpen={setPopupOpen}
+        width={width}
+        allergens={allergens}
+        setAllergens={setAllergens}
+        handleSave={handleSave}
+      />
+      <DeletionConfirmationModal
+        open={openConfirmDeleteAllergens}
+        setOpen={setOpenConfirmDeleteAllergens}
+        itemToDelete="Allergens"
+        actionButtonFunction={
+          stateOfDeleteAllergens == 'deactivate' ? handleDeactivate : handleReactivate
+        }
+        actionType={stateOfDeleteAllergens}
+      />
     </Panel>
   );
 };
