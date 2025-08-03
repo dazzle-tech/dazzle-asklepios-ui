58a12114370f217094486cdbc9b50ce3d87feeb2
hanan253yahya <165596621+hanan253yahya@users.noreply.github.com>
2025-08-03 11:12:27 +0300

    as-1805,1958

---
+++ src/pages/recovery/AnesthesiaRecovery.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+const AnesthesiaRecovery = () => {
+  return (
+    <div>
+      <h1>Anesthesia Recovery</h1>
+      {/* Additional content can be added here */}
+    </div>
+  );
+}
+export default AnesthesiaRecovery;
