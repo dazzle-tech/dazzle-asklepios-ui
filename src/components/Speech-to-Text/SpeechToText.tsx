7fb6ad2068a209aa6ff1ea05cb6860f15f0d9a7f
Rawan-Yahya200 <rawanyahya300@gmail.com>
2025-08-13 15:06:43 +0300

    Features/AS-2062

--- src/components/Speech-to-Text/SpeechToText.tsx
+++ src/components/Speech-to-Text/SpeechToText.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MyInput from '../MyInput';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMicrophone } from '@fortawesome/free-solid-svg-icons';
@@ -9,11 +9,18 @@ import './styles.less';
 
 const SpeechToText = ({ record, setRecord, fieldName, ...props }) => {
   const fieldLabel = props?.fieldLabel ?? camelCaseToLabel(fieldName);
-  
+    const [recording, setRecording] = useState(false);
+
   // handle clear textarea
   const handleClear = () => {
     setRecord({ ...record, [fieldName]: '' });
   };
+
+  // change recording state
+  const changeRecordingState = () => {
+    setRecording(!recording);
+  };
+
   return (
     <div
       className="container-of-speech-to-text"
@@ -23,9 +30,19 @@ const SpeechToText = ({ record, setRecord, fieldName, ...props }) => {
     >
       <div className="container-of-header-speech-to-text">
         <Translate>{fieldLabel}</Translate>
-        <FontAwesomeIcon icon={faMicrophone} className="icons-style" />
+        <div
+          className={`icons-style ${recording ? 'recording' : ''}`}
+          onClick={changeRecordingState}
+          style={{position: 'relative' }}
+        >
+          <FontAwesomeIcon icon={faMicrophone} />
+          {recording && (
+            <span className="pulse-ring"></span>
+          )}
+        </div>
       </div>
 
+       <div>
       <MyInput
         fieldType="textarea"
         fieldName={fieldName}
@@ -33,7 +50,10 @@ const SpeechToText = ({ record, setRecord, fieldName, ...props }) => {
         setRecord={setRecord}
         width={props?.width ?? 200}
         showLabel={false}
+        height="100%"
+        disabled={props.disabled ?? false}
       />
+      </div>
       <div className="container-of-button-speech-to-text">
         <MyButton appearance="ghost" color="#787777ff" onClick={handleClear}>
           Clear
--- src/pages/encounter/encounter-component/doctor-round/NewRound/ProgressNotes.tsx
+++ src/pages/encounter/encounter-component/doctor-round/NewRound/ProgressNotes.tsx
@@ -1,39 +1,37 @@
 import React from 'react';
-import { Form, Row } from 'rsuite';
-import MyInput from '@/components/MyInput';
+import { Form } from 'rsuite';
+import SpeechToText from '@/components/Speech-to-Text';
 const ProgressNotes = ({ doctorRound, setDoctorRound, doctorRoundList }) => {
   return (
-    <Row gutter={30}>
-      <Form fluid>
-        <MyInput
+      <Form>
+        <SpeechToText
           width="100%"
           fieldLabel="Initial Note"
           fieldName="initialNote"
-          fieldType="textarea"
           record={doctorRound}
           setRecord={setDoctorRound}
           disabled={!doctorRound?.key || (doctorRoundList?.object?.[0]?.initialNote ?? '') !== ''}
         />
-        <MyInput
+        <br/>
+        <SpeechToText
           width="100%"
           fieldLabel="Progress Note"
           fieldName="progressNote"
-          fieldType="textarea"
           record={doctorRound}
           setRecord={setDoctorRound}
           disabled={!doctorRound?.key}
         />
-        <MyInput
+        <br/>
+        <SpeechToText
           width="100%"
           fieldLabel="Special Event Note"
           fieldName="specialEventNote"
-          fieldType="textarea"
           record={doctorRound}
           setRecord={setDoctorRound}
           disabled={!doctorRound?.key}
         />
-      </Form>
-    </Row>
+        </Form>
+  
   );
 };
 export default ProgressNotes;
