a41d51c8fce10cb20d20d8196bb70b882bfe060a
Qaisnfi7 <qais.farouk@gmail.com>
2025-09-09 23:07:21 +0300

    2259

--- src/pages/encounter/encounter-list/EncounterList.tsx
+++ src/pages/encounter/encounter-list/EncounterList.tsx
@@ -58,7 +58,11 @@ const EncounterList = () => {
   const [encounterStatus, setEncounterStatus] = useState({ key: '' });
   const [startEncounter] = useStartEncounterMutation();
   const [cancelEncounter] = useCancelEncounterMutation();
+  //
+  const { data: bookVisitLovQueryResponse } = useGetLovValuesByCodeQuery('BOOK_VISIT_TYPE');
+  const { data: EncPriorityLovQueryResponse } = useGetLovValuesByCodeQuery('ENC_PRIORITY');
   const { data: encounterStatusLov } = useGetLovValuesByCodeQuery('ENC_STATUS');
+
   const [listRequest, setListRequest] = useState<ListRequest>({
     ...initialListRequest,
     ignore: true,
@@ -521,8 +525,70 @@ const EncounterList = () => {
             setRecord={setEncounterStatus}
           />
         </Form>
-
-        <AdvancedSearchFilters searchFilter={true} />
+        <AdvancedSearchFilters
+          searchFilter={true}
+          content={
+            <div className="advanced-filters">
+              <Form fluid className="dissss">
+                {/* Visit Type LOV */}
+                <MyInput
+                  fieldName="accessTypeLkey"
+                  fieldType="select"
+                  selectData={bookVisitLovQueryResponse?.object ?? []}
+                  selectDataLabel="lovDisplayVale"
+                  fieldLabel="Visit Type"
+                  selectDataValue="key"
+                  record={record}
+                  setRecord={setRecord}
+                  searchable={false}
+                />
+                {/* Chief Complain Text */}
+                <MyInput
+                  fieldName="chiefComplain"
+                  fieldType="text"
+                  record={record}
+                  setRecord={setRecord}
+                  fieldLabel="Chief Complain"
+                />
+                {/* Checkboxes*/}
+                <MyInput
+                  fieldName="withPrescription"
+                  fieldType="checkbox"
+                  record={record}
+                  setRecord={setRecord}
+                  label="With Prescription"
+                />
+                <MyInput
+                  fieldName="hasOrders"
+                  fieldType="checkbox"
+                  record={record}
+                  setRecord={setRecord}
+                  label="Has Orders"
+                />
+                <MyInput
+                  fieldName="isObserved"
+                  fieldType="checkbox"
+                  record={record}
+                  setRecord={setRecord}
+                  label="Is Observed"
+                />
+                {/* Priority LOV */}
+                <MyInput
+                  fieldName="priority"
+                  fieldType="select"
+                  record={record}
+                  setRecord={setRecord}
+                  selectData={EncPriorityLovQueryResponse?.object ?? []}
+                  selectDataLabel="lovDisplayVale"
+                  selectDataValue="key"
+                  placeholder="Select Priority"
+                  fieldLabel="Priority"
+                  searchable={false}
+                />
+              </Form>
+            </div>
+          }
+        />
       </>
     );
   };
