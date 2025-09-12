e6ac1704a399630656033310e7f06a615c8bec6d
Qaisnfi7 <qais.farouk@gmail.com>
2025-09-12 19:37:33 +0300

    2172

---
+++ src/pages/encounter/encounter-component/order-details/OrderDetails.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const OrderDetails = () => {
+  return (
+    <>
+      <div>
+        <p>order details</p>
+      </div>
+    </>
+  );
+};
+
+export default OrderDetails;
