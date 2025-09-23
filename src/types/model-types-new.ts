d03e69f7d96bc03ba14c0d3bab8f5c1b27f1800e
bushramashaiekh <bushramashaie5@gmail.com>
2025-09-23 13:38:53 +0300

    Facility and Role

--- src/types/model-types-new.ts
+++ src/types/model-types-new.ts
@@ -64,7 +64,6 @@ export interface Facility {
 	defaultCurrency:string;
   isActive?: boolean;
 } 
-
 export interface CreateFacility { 
 	name?:string;
 	emailAddress?:string;
@@ -85,5 +84,5 @@ export interface Role {
 export interface UserRole { 
 	roleId?:string;
   userId?:string;
-
 } 
+
