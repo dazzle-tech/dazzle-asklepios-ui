1ee616cd778882808e8d1cb9891e8c042c4eb5e0
Qaisnfi7 <qais.farouk@gmail.com>
2025-09-29 13:58:41 +0300

    2393

--- src/pages/encounter/encounter-component/cardiology/echo-doppler-test/EchoSections/DopplerValves.tsx
+++ src/pages/encounter/encounter-component/cardiology/echo-doppler-test/EchoSections/DopplerValves.tsx
@@ -1,7 +1,6 @@
 // pages/EchoSections/DopplerValves.tsx
 
 import React from 'react';
-import Section from '@/components/Section';
 import MyInput from '@/components/MyInput';
 import '../style.less';
 import SectionContainer from '@/components/SectionsoContainer';
@@ -18,17 +17,116 @@ const DopplerValves: React.FC<Props> = ({ record, setRecord }) => {
       content={
         <div className="handle-inputs-positions-size">
           <div className="for-right-adons-position-align">
-            <MyInput fieldLabel="Aortic Valve Peak Velocity" fieldName="aorticValvePeakVelocity" fieldType="number" rightAddon="m/s" width={200} record={record} setRecord={setRecord} rightAddonwidth={'auto'} />
-            <MyInput fieldLabel="Aortic Valve Mean Gradient" fieldName="aorticValveMeanGradient" fieldType="number" rightAddon="mmHg" width={180} record={record} setRecord={setRecord} rightAddonwidth={'auto'} />
-            <MyInput fieldLabel="Aortic Valve Area" fieldName="aorticValveArea" fieldType="number" rightAddon="cm²" width={200} record={record} setRecord={setRecord} rightAddonwidth={'auto'} />
-            <MyInput fieldLabel="Mitral Valve E Velocity" fieldName="mitralValveEVelocity" fieldType="number" rightAddon="m/s" width={200} record={record} setRecord={setRecord} rightAddonwidth={'auto'} />
-            <MyInput fieldLabel="Mitral Valve A Velocity" fieldName="mitralValveAVelocity" fieldType="number" rightAddon="m/s" width={200} record={record} setRecord={setRecord} rightAddonwidth={'auto'} />
-            <MyInput fieldLabel="E/A Ratio" fieldName="eaRatio" fieldType="number" rightAddon="ratio" width={200} record={record} setRecord={setRecord} />
-            <MyInput fieldLabel="Mitral Valve Deceleration Time" fieldName="mitralValveDecelerationTime" fieldType="number" rightAddon="ms" width={200} record={record} setRecord={setRecord} rightAddonwidth={'auto'} />
-            <MyInput fieldLabel="Mitral Valve Area" fieldName="mitralValveArea" fieldType="number" rightAddon="cm²" width={200} record={record} setRecord={setRecord} />
-            <MyInput fieldLabel="Tricuspid Regurgitation Peak Velocity" fieldName="tricuspidRegurgitationPeakVelocity" fieldType="number" rightAddon="m/s" width={200} record={record} setRecord={setRecord} rightAddonwidth={'auto'} />
-            <MyInput fieldLabel="Estimated PASP" fieldName="estimatedPASP" fieldType="number" rightAddon="mmHg" width={180} record={record} setRecord={setRecord} rightAddonwidth={'auto'} />
-            <MyInput fieldLabel="Pulmonic Valve Peak Velocity" fieldName="pulmonicValvePeakVelocity" fieldType="number" rightAddon="m/s" width={480} record={record} setRecord={setRecord} rightAddonwidth={'auto'} />
+            <MyInput
+              fieldLabel="Aortic Valve Peak Velocity"
+              fieldName="aorticValvePeakVelocity"
+              fieldType="number"
+              rightAddon="m/s"
+              width={170}
+              record={record}
+              setRecord={setRecord}
+              rightAddonwidth={'auto'}
+            />
+            <MyInput
+              fieldLabel="Aortic Valve Mean Gradient"
+              fieldName="aorticValveMeanGradient"
+              fieldType="number"
+              rightAddon="mmHg"
+              width={160}
+              record={record}
+              setRecord={setRecord}
+              rightAddonwidth={'auto'}
+            />
+            <MyInput
+              fieldLabel="Aortic Valve Area"
+              fieldName="aorticValveArea"
+              fieldType="number"
+              rightAddon="cm²"
+              width={170}
+              record={record}
+              setRecord={setRecord}
+              rightAddonwidth={'auto'}
+            />
+            <MyInput
+              fieldLabel="Mitral Valve E Velocity"
+              fieldName="mitralValveEVelocity"
+              fieldType="number"
+              rightAddon="m/s"
+              width={170}
+              record={record}
+              setRecord={setRecord}
+              rightAddonwidth={'auto'}
+            />
+            <MyInput
+              fieldLabel="Mitral Valve A Velocity"
+              fieldName="mitralValveAVelocity"
+              fieldType="number"
+              rightAddon="m/s"
+              width={170}
+              record={record}
+              setRecord={setRecord}
+              rightAddonwidth={'auto'}
+            />
+            <MyInput
+              fieldLabel="E/A Ratio"
+              fieldName="eaRatio"
+              fieldType="number"
+              rightAddon="ratio"
+              width={170}
+              record={record}
+              setRecord={setRecord}
+              rightAddonwidth={'auto'}
+            />
+            <MyInput
+              fieldLabel="Mitral Valve Deceleration Time"
+              fieldName="mitralValveDecelerationTime"
+              fieldType="number"
+              rightAddon="ms"
+              width={170}
+              record={record}
+              setRecord={setRecord}
+              rightAddonwidth={'auto'}
+            />
+            <MyInput
+              fieldLabel="Mitral Valve Area"
+              fieldName="mitralValveArea"
+              fieldType="number"
+              rightAddon="cm²"
+              width={160}
+              record={record}
+              setRecord={setRecord}
+              rightAddonwidth={'auto'}
+            />
+            <MyInput
+              fieldLabel="Tricuspid Regurgitation Peak Velocity"
+              fieldName="tricuspidRegurgitationPeakVelocity"
+              fieldType="number"
+              rightAddon="m/s"
+              width={170}
+              record={record}
+              setRecord={setRecord}
+              rightAddonwidth={'auto'}
+            />
+            <MyInput
+              fieldLabel="Estimated PASP"
+              fieldName="estimatedPASP"
+              fieldType="number"
+              rightAddon="mmHg"
+              width={160}
+              record={record}
+              setRecord={setRecord}
+              rightAddonwidth={'auto'}
+            />
+            <MyInput
+              fieldLabel="Pulmonic Valve Peak Velocity"
+              fieldName="pulmonicValvePeakVelocity"
+              fieldType="number"
+              rightAddon="m/s"
+              width={440}
+              record={record}
+              setRecord={setRecord}
+              rightAddonwidth={'auto'}
+            />
           </div>
         </div>
       }
--- src/pages/encounter/encounter-component/cardiology/echo-doppler-test/EchoSections/Measurements.tsx
+++ src/pages/encounter/encounter-component/cardiology/echo-doppler-test/EchoSections/Measurements.tsx
@@ -1,10 +1,7 @@
-// pages/EchoSections/Measurements.tsx
-
 import React from 'react';
-import Section from '@/components/Section';
 import MyInput from '@/components/MyInput';
-import '../style.less';
 import SectionContainer from '@/components/SectionsoContainer';
+import '../style.less';
 
 interface Props {
   record: any;
@@ -14,20 +11,110 @@ interface Props {
 const Measurements: React.FC<Props> = ({ record, setRecord }) => {
   return (
     <SectionContainer
-      title="Measurements – M-mode / 2D"
+      title="Measurements - M-mode / 2D"
       content={
         <div className="handle-inputs-positions-size">
           <div className="for-right-adons-position-align">
-            <MyInput fieldLabel="LVEDD" fieldName="LVEDD" fieldType="number" rightAddon="mm" width={200} record={record} setRecord={setRecord} rightAddonwidth={'auto'} />
-            <MyInput fieldLabel="LVESD" fieldName="LVESD" fieldType="number" rightAddon="mm" width={200} record={record} setRecord={setRecord} rightAddonwidth={'auto'} />
-            <MyInput fieldLabel="IVS Thickness" fieldName="IVSThickness" fieldType="number" rightAddon="mm" width={200} record={record} setRecord={setRecord} rightAddonwidth={'auto'} />
-            <MyInput fieldLabel="Posterior Wall Thickness" fieldName="posteriorWallThickness" fieldType="number" rightAddon="mm" width={200} record={record} setRecord={setRecord} rightAddonwidth={'auto'} />
-            <MyInput fieldLabel="LVEF" fieldName="LVEF" fieldType="number" rightAddon="%" width={200} record={record} setRecord={setRecord} rightAddonwidth={'auto'} />
-            <MyInput fieldLabel="LV Mass Index" fieldName="LVMassIndex" fieldType="number" rightAddon="g/m²" width={200} record={record} setRecord={setRecord} rightAddonwidth={'auto'} />
-            <MyInput fieldLabel="LA Diameter" fieldName="LADiameter" fieldType="number" rightAddon="mm" width={200} record={record} setRecord={setRecord} rightAddonwidth={'auto'} />
-            <MyInput fieldLabel="RA Area" fieldName="RAArea" fieldType="number" rightAddon="cm²" width={200} record={record} setRecord={setRecord} rightAddonwidth={'auto'} />
-            <MyInput fieldLabel="RV Diameter" fieldName="RVDiameter" fieldType="number" rightAddon="mm" width={200} record={record} setRecord={setRecord} rightAddonwidth={'auto'} />
-            <MyInput fieldLabel="Ascending Aorta Diameter" fieldName="ascendingAortaDiameter" fieldType="number" rightAddon="mm" width={200} record={record} setRecord={setRecord} rightAddonwidth={'auto'} />
+            <MyInput
+              fieldLabel="LVEDD"
+              fieldName="LVEDD"
+              fieldType="number"
+              rightAddon="mm"
+              width={160}
+              record={record}
+              setRecord={setRecord}
+              rightAddonwidth={'auto'}
+            />
+            <MyInput
+              fieldLabel="LVESD"
+              fieldName="LVESD"
+              fieldType="number"
+              rightAddon="mm"
+              width={160}
+              record={record}
+              setRecord={setRecord}
+              rightAddonwidth={'auto'}
+            />
+            <MyInput
+              fieldLabel="IVS Thickness"
+              fieldName="IVSThickness"
+              fieldType="number"
+              rightAddon="mm"
+              width={160}
+              record={record}
+              setRecord={setRecord}
+              rightAddonwidth={'auto'}
+            />
+            <MyInput
+              fieldLabel="Posterior Wall Thickness"
+              fieldName="posteriorWallThickness"
+              fieldType="number"
+              rightAddon="mm"
+              width={160}
+              record={record}
+              setRecord={setRecord}
+              rightAddonwidth={'auto'}
+            />
+            <MyInput
+              fieldLabel="LVEF"
+              fieldName="LVEF"
+              fieldType="number"
+              rightAddon="%"
+              width={160}
+              record={record}
+              setRecord={setRecord}
+              rightAddonwidth={'auto'}
+            />
+            <MyInput
+              fieldLabel="LV Mass Index"
+              fieldName="LVMassIndex"
+              fieldType="number"
+              rightAddon="g/m²"
+              width={160}
+              record={record}
+              setRecord={setRecord}
+              rightAddonwidth={'auto'}
+            />
+            <MyInput
+              fieldLabel="LA Diameter"
+              fieldName="LADiameter"
+              fieldType="number"
+              rightAddon="mm"
+              width={160}
+              record={record}
+              setRecord={setRecord}
+              rightAddonwidth={'auto'}
+            />
+            <MyInput
+              fieldLabel="RA Area"
+              fieldName="RAArea"
+              fieldType="number"
+              rightAddon="cm²"
+              width={160}
+              record={record}
+              setRecord={setRecord}
+              rightAddonwidth={'auto'}
+            />
+            <MyInput
+              fieldLabel="RV Diameter"
+              fieldName="RVDiameter"
+              fieldType="number"
+              rightAddon="mm"
+              width={160}
+              record={record}
+              setRecord={setRecord}
+              rightAddonwidth={'auto'}
+            />
+            <MyInput
+              fieldLabel="Ascending Aorta Diameter"
+              fieldName="ascendingAortaDiameter"
+              fieldType="number"
+              rightAddon="mm"
+              width={160}
+              record={record}
+              setRecord={setRecord}
+              rightAddonwidth={'auto'}
+            />
           </div>
         </div>
       }
