9a465f71a03e57112a5c2ed0cce01bd1242a42fe
Rawan-Yahya200 <rawanyahya300@gmail.com>
2025-10-04 21:50:06 +0300

    Fixes-2431

--- src/pages/encounter/encounter-component/doctor-round/NewRound/ProgressNotes.tsx
+++ src/pages/encounter/encounter-component/doctor-round/NewRound/ProgressNotes.tsx
@@ -1,39 +1,43 @@
 import React from 'react';
 import { Form } from 'rsuite';
-import SpeechToText from '@/components/Speech-to-Text';
+import MyInput from '@/components/MyInput';
 const ProgressNotes = ({ doctorRound, setDoctorRound, doctorRoundList, ...props }) => {
-
- 
   return (
-      <Form>
-        <SpeechToText
-          width="100%"
-          fieldLabel="Initial Note"
-          fieldName="initialNote"
-          record={doctorRound}
-          setRecord={setDoctorRound}
-          disabled={!doctorRound?.key || (doctorRoundList?.object?.[0]?.initialNote ?? '') !== '' || props.view}
-        />
-        <br/>
-        <SpeechToText
-          width="100%"
-          fieldLabel="Progress Note"
-          fieldName="progressNote"
-          record={doctorRound}
-          setRecord={setDoctorRound}
-          disabled={!doctorRound?.key || props.view}
-        />
-        <br/>
-        <SpeechToText
-          width="100%"
-          fieldLabel="Special Event Note"
-          fieldName="specialEventNote"
-          record={doctorRound}
-          setRecord={setDoctorRound}
-          disabled={!doctorRound?.key || props.view}
-        />
-        </Form>
-  
+    <Form>
+      <MyInput
+        width="100%"
+        fieldLabel="Initial Note"
+        fieldName="initialNote"
+        record={doctorRound}
+        setRecord={setDoctorRound}
+        disabled={
+          !doctorRound?.key ||
+          (doctorRoundList?.object?.[0]?.initialNote ?? '') !== '' ||
+          props.view
+        }
+        fieldType="textarea"
+      />
+      <br />
+      <MyInput
+        width="100%"
+        fieldLabel="Progress Note"
+        fieldName="progressNote"
+        record={doctorRound}
+        setRecord={setDoctorRound}
+        disabled={!doctorRound?.key || props.view}
+        fieldType="textarea"
+      />
+      <br />
+      <MyInput
+        width="100%"
+        fieldLabel="Special Event Note"
+        fieldName="specialEventNote"
+        record={doctorRound}
+        setRecord={setDoctorRound}
+        disabled={!doctorRound?.key || props.view}
+        fieldType="textarea"
+      />
+    </Form>
   );
 };
 export default ProgressNotes;
