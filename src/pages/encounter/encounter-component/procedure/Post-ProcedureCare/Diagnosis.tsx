ca6fede40bee91ef8fffce4f48f1a7374848e712
Qaisnfi7 <qais.farouk@gmail.com>
2025-10-21 14:52:39 +0300

    ASK-140,141,145

--- src/pages/encounter/encounter-component/procedure/Post-ProcedureCare/Diagnosis.tsx
+++ src/pages/encounter/encounter-component/procedure/Post-ProcedureCare/Diagnosis.tsx
@@ -1,155 +1,149 @@
-import React, { forwardRef, useImperativeHandle, useState } from "react";
-import { Col, Divider, Dropdown, Form, Input, InputGroup, Row, Text } from "rsuite";
+import MyInput from '@/components/MyInput';
+import SectionContainer from '@/components/SectionsoContainer';
+import { useAppDispatch } from '@/hooks';
+import { useSavePostProcedureCareMutation } from '@/services/procedureService';
+import { useGetIcdListQuery } from '@/services/setupService';
+import { newApPostProcedureCare } from '@/types/model-types-constructor';
+import { initialListRequest } from '@/types/types';
+import { notify } from '@/utils/uiReducerActions';
 import SearchIcon from '@rsuite/icons/Search';
-import { useGetIcdListQuery } from "@/services/setupService";
-import { initialListRequest } from "@/types/types";
-import MyInput from "@/components/MyInput";
-import { newApPostProcedureCare } from "@/types/model-types-constructor";
-import { useSavePostProcedureCareMutation } from "@/services/procedureService";
-import MyButton from "@/components/MyButton/MyButton";
-import { useAppDispatch } from "@/hooks";
-import { notify } from "@/utils/uiReducerActions";
+import React, { forwardRef, useImperativeHandle, useState } from 'react';
+import { Col, Dropdown, Form, Input, InputGroup, Row, Text } from 'rsuite';
 import '../styles.less';
 export type DiagRef = {
-    handleSave: () => void;
+  handleSave: () => void;
 };
 type DiagnosisProps = {
-    procedure: any,
-    user: any
+  procedure: any;
+  user: any;
 };
 const Diagnosis = forwardRef<DiagRef, DiagnosisProps>(({ procedure, user }, ref) => {
-
-    const dispatch = useAppDispatch();
-    const [diagno, setDiagno] = useState({ ...newApPostProcedureCare });
-    const [saveProcedureCare] = useSavePostProcedureCareMutation();
-    const [searchKeyword, setSearchKeyword] = useState('');
-    const [listIcdRequest, setListIcdRequest] = useState({ ...initialListRequest, pageSize: 1000 });
-    const { data: icdListResponseData } = useGetIcdListQuery(listIcdRequest);
-    const modifiedData = (icdListResponseData?.object ?? []).map(item => ({
-        ...item,
-        combinedLabel: `${item.icdCode} - ${item.description}`
-    }));
-    const handleSave = async () => {
-    
-        try {
-            const response = await saveProcedureCare({
-                ...diagno,
-                procedureKey: procedure?.key,
-                createdBy: user?.key,
-            }).unwrap();
-            dispatch(notify({ msg: 'Saved Successfully', sev: "success" }));
-        } catch (error) {
-            console.error("Error saving procedure care:", error);
-            dispatch(notify({ msg: 'Error saving procedure care', sev: "error" }));
-        }
-    };
-    useImperativeHandle(ref, () => ({
-       handleSave
-    }));
-    const handleSearch = value => {
-        setSearchKeyword(value);
-    };
-    return (
-        <div className='container-form' ref={ref}>
-            <div className='title-div'>
-                <Text>Procedure Care</Text>
-            </div>
-            <Divider />
+  const dispatch = useAppDispatch();
+  const [diagno, setDiagno] = useState({ ...newApPostProcedureCare });
+  const [saveProcedureCare] = useSavePostProcedureCareMutation();
+  const [searchKeyword, setSearchKeyword] = useState('');
+  const [listIcdRequest, setListIcdRequest] = useState({ ...initialListRequest, pageSize: 1000 });
+  const { data: icdListResponseData } = useGetIcdListQuery(listIcdRequest);
+  const modifiedData = (icdListResponseData?.object ?? []).map(item => ({
+    ...item,
+    combinedLabel: `${item.icdCode} - ${item.description}`
+  }));
+  const handleSave = async () => {
+    try {
+      const response = await saveProcedureCare({
+        ...diagno,
+        procedureKey: procedure?.key,
+        createdBy: user?.key
+      }).unwrap();
+      dispatch(notify({ msg: 'Saved Successfully', sev: 'success' }));
+    } catch (error) {
+      console.error('Error saving procedure care:', error);
+      dispatch(notify({ msg: 'Error saving procedure care', sev: 'error' }));
+    }
+  };
+  useImperativeHandle(ref, () => ({
+    handleSave
+  }));
+  const handleSearch = value => {
+    setSearchKeyword(value);
+  };
+  return (
+    <SectionContainer
+      title={<Text>Procedure Care</Text>}
+      content={
+        <Row>
+          <Col md={12}>
             <Row>
+              <Col md={24}>
+                <Text>Diagnosis</Text>
 
+                <div style={{ position: 'relative' }}>
+                  <InputGroup inside>
+                    <Input
+                      placeholder="Search ICD-10"
+                      value={searchKeyword}
+                      onChange={handleSearch}
+                    />
+                    <InputGroup.Button>
+                      <SearchIcon />
+                    </InputGroup.Button>
+                  </InputGroup>
 
-                <Col md={12}>
-                    <Row>
-                        <Col md={24}>
-                            <Text>Diagnosis</Text>
-
-
-                            <div style={{ position: 'relative' }}>
-                                <InputGroup inside>
-                                    <Input
-                                        placeholder="Search ICD-10"
-                                        value={searchKeyword}
-                                        onChange={handleSearch}
-                                    />
-                                    <InputGroup.Button>
-                                        <SearchIcon />
-                                    </InputGroup.Button>
-                                </InputGroup>
-
-                                {searchKeyword && (
-                                    <div
-                                        style={{
-                                            position: 'absolute',
-                                            top: '100%',
-                                            left: 0,
-                                            width: '100%',
-                                            maxHeight: '150px',
-                                            overflowY: 'auto',
-                                            zIndex: 9999,
-                                            backgroundColor: 'white',
-                                            border: '1px solid #ccc',
-                                            borderRadius: '4px',
-                                            boxShadow: '0 4px 10px rgba(0,0,0,0.1)',
-                                            marginTop: '4px',
-                                        }}
-                                    >
-                                        <Dropdown.Menu>
-                                            {modifiedData?.map((mod) => (
-                                                <Dropdown.Item
-                                                    key={mod.key}
-                                                    eventKey={mod.key}
-                                                    onClick={() => {
-                                                        setDiagno({
-                                                            ...diagno,
-                                                            diagnoseKey: mod.key,
-                                                        });
-                                                        setSearchKeyword('');
-                                                    }}
-                                                >
-                                                    <span>{mod.icdCode}</span>
-                                                    <span>&nbsp;&nbsp;</span>
-                                                    <span>{mod.description}</span>
-                                                </Dropdown.Item>
-                                            ))}
-                                        </Dropdown.Menu>
-                                    </div>
-                                )}
-                            </div>
-                        </Col>
-
-                    </Row>
-                    <Row>
-                        <Col md={24}>
-
-                            <InputGroup>
-                                <Input
-                                    disabled={true}
-                                    value={
-                                        diagno?.diagnoseKey
-                                            ? `${modifiedData?.find(item => item.key === diagno?.diagnoseKey)?.icdCode}, ${modifiedData?.find(item => item.key === diagno?.diagnoseKey)?.description}`
-                                            : ''
-                                    }
-                                />
-                            </InputGroup>
-                        </Col>
-                    </Row>
-
-                </Col>
-                <Col md={12}>
-                    <Form fluid>
-                        <MyInput
-                            width="100%"
-                            fieldType="textarea"
-                            fieldName='patientCondition'
-                            record={diagno}
-                            setRecord={setDiagno}
-                        />
-                    </Form>
-                </Col>
-
+                  {searchKeyword && (
+                    <div
+                      style={{
+                        position: 'absolute',
+                        top: '100%',
+                        left: 0,
+                        width: '100%',
+                        maxHeight: '150px',
+                        overflowY: 'auto',
+                        zIndex: 9999,
+                        backgroundColor: 'white',
+                        border: '1px solid #ccc',
+                        borderRadius: '4px',
+                        boxShadow: '0 4px 10px rgba(0,0,0,0.1)',
+                        marginTop: '4px'
+                      }}
+                    >
+                      <Dropdown.Menu>
+                        {modifiedData?.map(mod => (
+                          <Dropdown.Item
+                            key={mod.key}
+                            eventKey={mod.key}
+                            onClick={() => {
+                              setDiagno({
+                                ...diagno,
+                                diagnoseKey: mod.key
+                              });
+                              setSearchKeyword('');
+                            }}
+                          >
+                            <span>{mod.icdCode}</span>
+                            <span>&nbsp;&nbsp;</span>
+                            <span>{mod.description}</span>
+                          </Dropdown.Item>
+                        ))}
+                      </Dropdown.Menu>
+                    </div>
+                  )}
+                </div>
+              </Col>
             </Row>
-          
-        </div>
-    )
+            <Row>
+              <Col md={24}>
+                <InputGroup>
+                  <Input
+                    disabled={true}
+                    value={
+                      diagno?.diagnoseKey
+                        ? `${
+                            modifiedData?.find(item => item.key === diagno?.diagnoseKey)?.icdCode
+                          }, ${
+                            modifiedData?.find(item => item.key === diagno?.diagnoseKey)
+                              ?.description
+                          }`
+                        : ''
+                    }
+                  />
+                </InputGroup>
+              </Col>
+            </Row>
+          </Col>
+          <Col md={12}>
+            <Form fluid>
+              <MyInput
+                width="100%"
+                fieldType="textarea"
+                fieldName="patientCondition"
+                record={diagno}
+                setRecord={setDiagno}
+              />
+            </Form>
+          </Col>
+        </Row>
+      }
+    />
+  );
 });
 export default Diagnosis;
--- src/pages/encounter/encounter-component/procedure/Post-ProcedureCare/PostProcedureAnesthesia.tsx
+++ src/pages/encounter/encounter-component/procedure/Post-ProcedureCare/PostProcedureAnesthesia.tsx
@@ -1,21 +1,23 @@
-import MyButton from "@/components/MyButton/MyButton";
-import MyInput from "@/components/MyInput";
-import { useAppDispatch } from "@/hooks";
-import { useSavePostProcedurAnesthesiaMutation } from "@/services/procedureService";
-import { useGetLovValuesByCodeQuery } from "@/services/setupService";
-import { newApPostProcedureAnesthesia } from "@/types/model-types-constructor";
-import { notify } from "@/utils/uiReducerActions";
-import { set } from "lodash";
-import React, { forwardRef, useEffect, useImperativeHandle, useState } from "react";
-import { Col, Divider, Form, Row, Text } from "rsuite";
+import MyButton from '@/components/MyButton/MyButton';
+import MyInput from '@/components/MyInput';
+import SectionContainer from '@/components/SectionsoContainer';
+import { useAppDispatch } from '@/hooks';
+import { useSavePostProcedurAnesthesiaMutation } from '@/services/procedureService';
+import { useGetLovValuesByCodeQuery } from '@/services/setupService';
+import { newApPostProcedureAnesthesia } from '@/types/model-types-constructor';
+import { notify } from '@/utils/uiReducerActions';
+import { set } from 'lodash';
+import React, { forwardRef, useEffect, useImperativeHandle, useState } from 'react';
+import { Col, Divider, Form, Row, Text } from 'rsuite';
 export type anesthsiaRef = {
-    handleSave: () => void;
+  handleSave: () => void;
 };
 type AnesthesiaProps = {
-    procedure: any,
-    user: any
+  procedure: any;
+  user: any;
 };
-const PostProcedureAnesthesia= forwardRef<anesthsiaRef, AnesthesiaProps>(({ procedure, user, ...props }, ref) => {
+const PostProcedureAnesthesia = forwardRef<anesthsiaRef, AnesthesiaProps>(
+  ({ procedure, user, ...props }, ref) => {
     const dispatch = useAppDispatch();
     // Fetching the LOV values for Aldrete score components
     const { data: oxsatQueryResponse } = useGetLovValuesByCodeQuery('ALDRETE_OXSAT');
@@ -24,133 +26,144 @@ const PostProcedureAnesthesia= forwardRef<anesthsiaRef, AnesthesiaProps>(({ proc
     const { data: respirLovQueryResponse } = useGetLovValuesByCodeQuery('ALDRETE_RESPIR');
     const { data: activityLovQueryResponse } = useGetLovValuesByCodeQuery('ALDRETE_ACTIVITY');
     const [anesthesia, setAnesthesia] = useState({ ...newApPostProcedureAnesthesia });
-    const [saveAnesthesia,saveAnesthesiaMutation] = useSavePostProcedurAnesthesiaMutation();
-//calculate aldrete score based on the selected values
-  useEffect(() => {
-    const oxsatscore = oxsatQueryResponse?.object?.find(item => item.key === anesthesia?.oxygenSaturationLkey)?.score || 0;
-    const conscscore = conscLovQueryResponse?.object?.find(item => item.key === anesthesia?.consciousnessLkey)?.score || 0;
-    const circuscore = circuLovQueryResponse?.object?.find(item => item.key === anesthesia?.circulationLkey)?.score || 0;
-    const respirscore = respirLovQueryResponse?.object?.find(item => item.key === anesthesia?.respirationLkey)?.score || 0;
-    const activityscore = activityLovQueryResponse?.object?.find(item => item.key === anesthesia?.activityLkey)?.score || 0;
+    const [saveAnesthesia, saveAnesthesiaMutation] = useSavePostProcedurAnesthesiaMutation();
+    //calculate aldrete score based on the selected values
+    useEffect(() => {
+      const oxsatscore =
+        oxsatQueryResponse?.object?.find(item => item.key === anesthesia?.oxygenSaturationLkey)
+          ?.score || 0;
+      const conscscore =
+        conscLovQueryResponse?.object?.find(item => item.key === anesthesia?.consciousnessLkey)
+          ?.score || 0;
+      const circuscore =
+        circuLovQueryResponse?.object?.find(item => item.key === anesthesia?.circulationLkey)
+          ?.score || 0;
+      const respirscore =
+        respirLovQueryResponse?.object?.find(item => item.key === anesthesia?.respirationLkey)
+          ?.score || 0;
+      const activityscore =
+        activityLovQueryResponse?.object?.find(item => item.key === anesthesia?.activityLkey)
+          ?.score || 0;
 
-    const aldreteScore = oxsatscore + conscscore + circuscore + respirscore + activityscore;
-   
+      const aldreteScore = oxsatscore + conscscore + circuscore + respirscore + activityscore;
 
-    setAnesthesia(prev => ({
+      setAnesthesia(prev => ({
         ...prev,
         aldreteScore
-    }));
-}, [
-    anesthesia?.oxygenSaturationLkey,
-    anesthesia?.consciousnessLkey,
-    anesthesia?.circulationLkey,
-    anesthesia?.respirationLkey,
-    anesthesia?.activityLkey,
-    oxsatQueryResponse,
-    conscLovQueryResponse,
-    circuLovQueryResponse,
-    respirLovQueryResponse,
-    activityLovQueryResponse
-]);
-// Update the anesthesia state when saveAnesthesiaMutation changes
-    useEffect(()=>{
-        
-        setAnesthesia(saveAnesthesiaMutation?.data || newApPostProcedureAnesthesia);
-    },[saveAnesthesiaMutation]);
-// Function to handle saving the anesthesia data
+      }));
+    }, [
+      anesthesia?.oxygenSaturationLkey,
+      anesthesia?.consciousnessLkey,
+      anesthesia?.circulationLkey,
+      anesthesia?.respirationLkey,
+      anesthesia?.activityLkey,
+      oxsatQueryResponse,
+      conscLovQueryResponse,
+      circuLovQueryResponse,
+      respirLovQueryResponse,
+      activityLovQueryResponse
+    ]);
+    // Update the anesthesia state when saveAnesthesiaMutation changes
+    useEffect(() => {
+      setAnesthesia(saveAnesthesiaMutation?.data || newApPostProcedureAnesthesia);
+    }, [saveAnesthesiaMutation]);
+    // Function to handle saving the anesthesia data
     const handleSave = async () => {
-        try {
-            const response = await saveAnesthesia({
-                ...anesthesia,
-                procedureKey: procedure?.key,
-                createdBy: user?.key,
-            }).unwrap();
-            dispatch(notify({ msg: 'Saved Anesthesia Successfully', sev: "success" }));
-        } catch (error) {
-            dispatch(notify({ msg: 'Error saving anesthesia', sev: "error" }));
-        }
-    }
+      try {
+        const response = await saveAnesthesia({
+          ...anesthesia,
+          procedureKey: procedure?.key,
+          createdBy: user?.key
+        }).unwrap();
+        dispatch(notify({ msg: 'Saved Anesthesia Successfully', sev: 'success' }));
+      } catch (error) {
+        dispatch(notify({ msg: 'Error saving anesthesia', sev: 'error' }));
+      }
+    };
     // use to
     useImperativeHandle(ref, () => ({
-             handleSave
-          }));
-    return ( <div  className='container-form' ref={ref}>
-            <div  className='title-div'>
-                <Text>Post-Procedure Anesthesia</Text> 
-            </div>
-            <Divider />
-        <Row gutter={15} className="r">
+      handleSave
+    }));
+    return (
+      <SectionContainer
+        title={<Text>Post-Procedure Anesthesia</Text>}
+        content={
+          <Row gutter={15} className="r">
             <Form fluid>
-                <Col md={12}>
-                    <MyInput
-                        width="100%"
-                        da selectData={oxsatQueryResponse?.object ?? []}
-                        selectDataLabel="lovDisplayVale"
-                        selectDataValue="key"
-                        fieldType="select"
-                        fieldName={"oxygenSaturationLkey"}
-                        record={anesthesia}
-                        setRecord={setAnesthesia}
-                        searchable={false}
-                    />
-                     <MyInput
-                        width="100%"
-                        selectData={conscLovQueryResponse?.object ?? []}
-                        selectDataLabel="lovDisplayVale"
-                        selectDataValue="key"
-                        fieldType="select"
-                        fieldName={"consciousnessLkey"}
-                        record={anesthesia}
-                        setRecord={setAnesthesia}
-                        searchable={false}
-                        />
-
-                    <MyInput
-                        width="100%"
-                        selectData={circuLovQueryResponse?.object ?? []}
-                        selectDataLabel="lovDisplayVale"
-                        selectDataValue="key"
-                        fieldType="select"
-                        fieldName={"circulationLkey"}
-                        record={anesthesia}
-                        setRecord={setAnesthesia}
-                        searchable={false}/>
-
-                </Col>
-                <Col md={12}>
+              <Col md={12}>
                 <MyInput
-                        width="100%"
-                        selectData={respirLovQueryResponse?.object ?? []}
-                        selectDataLabel="lovDisplayVale"
-                        selectDataValue="key"
-                        fieldType="select"
-                        fieldName={"respirationLkey"}
-                        record={anesthesia}
-                        setRecord={setAnesthesia}
-                        searchable={false}/>
+                  width="100%"
+                  da
+                  selectData={oxsatQueryResponse?.object ?? []}
+                  selectDataLabel="lovDisplayVale"
+                  selectDataValue="key"
+                  fieldType="select"
+                  fieldName={'oxygenSaturationLkey'}
+                  record={anesthesia}
+                  setRecord={setAnesthesia}
+                  searchable={false}
+                />
+                <MyInput
+                  width="100%"
+                  selectData={conscLovQueryResponse?.object ?? []}
+                  selectDataLabel="lovDisplayVale"
+                  selectDataValue="key"
+                  fieldType="select"
+                  fieldName={'consciousnessLkey'}
+                  record={anesthesia}
+                  setRecord={setAnesthesia}
+                  searchable={false}
+                />
 
-                    <MyInput
-                        width="100%"
-                        selectData={activityLovQueryResponse?.object ?? []}
-                        selectDataLabel="lovDisplayVale"
-                        selectDataValue="key"
-                        fieldType="select"
-                        fieldName={"activityLkey"}
-                        record={anesthesia}
-                        setRecord={setAnesthesia}
-                        searchable={false}
-                        />
-                    <MyInput
-                        width="100%"
-                        fieldType="number"
-                        
-                        fieldName='aldreteScore'
-                        record={anesthesia}
-                        setRecord={setAnesthesia}/>
+                <MyInput
+                  width="100%"
+                  selectData={circuLovQueryResponse?.object ?? []}
+                  selectDataLabel="lovDisplayVale"
+                  selectDataValue="key"
+                  fieldType="select"
+                  fieldName={'circulationLkey'}
+                  record={anesthesia}
+                  setRecord={setAnesthesia}
+                  searchable={false}
+                />
+              </Col>
+              <Col md={12}>
+                <MyInput
+                  width="100%"
+                  selectData={respirLovQueryResponse?.object ?? []}
+                  selectDataLabel="lovDisplayVale"
+                  selectDataValue="key"
+                  fieldType="select"
+                  fieldName={'respirationLkey'}
+                  record={anesthesia}
+                  setRecord={setAnesthesia}
+                  searchable={false}
+                />
 
-                </Col></Form>
-              
-        </Row>
-    </div>);
-});
+                <MyInput
+                  width="100%"
+                  selectData={activityLovQueryResponse?.object ?? []}
+                  selectDataLabel="lovDisplayVale"
+                  selectDataValue="key"
+                  fieldType="select"
+                  fieldName={'activityLkey'}
+                  record={anesthesia}
+                  setRecord={setAnesthesia}
+                  searchable={false}
+                />
+                <MyInput
+                  width="100%"
+                  fieldType="number"
+                  fieldName="aldreteScore"
+                  record={anesthesia}
+                  setRecord={setAnesthesia}
+                />
+              </Col>
+            </Form>
+          </Row>
+        }
+      />
+    );
+  }
+);
 export default PostProcedureAnesthesia;
