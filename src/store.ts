1dfea171934c1a8d002800a0308ce0c52f8fa87b
sondosabdalaal12 <engsondosabdalaal@outlook.com>
2025-11-11 14:07:37 +0200

    Update store.ts

--- src/store.ts
+++ src/store.ts
@@ -12,6 +12,7 @@ import { encounterService } from '@/services/encounterService';
 import { dentalService } from '@/services/dentalService';
 import { observationService } from '@/services/observationService';
 import { medicationsSetupService } from './services/medicationsSetupService';
+import { activeIngredientAdverseEffectService } from '@/services/setup/activeIngredients/activeIngredientAdverseEffectService';
 import { attachmentService } from '@/services/attachmentService';
 import { appointmentService } from './services/appointmentService';
 import { userService } from '@/services/userService';
@@ -80,6 +81,7 @@ export const store = configureStore({
 
     //medication
     [medicationsSetupService.reducerPath]: medicationsSetupService.reducer,
+    [activeIngredientAdverseEffectService.reducerPath]: activeIngredientAdverseEffectService.reducer,
 
     //account
     [accountApi.reducerPath]: accountApi.reducer,
@@ -181,6 +183,7 @@ export const store = configureStore({
       inventoryService.middleware,
       setupService.middleware,
       medicationsSetupService.middleware,
+      activeIngredientAdverseEffectService.middleware,
       appointmentService.middleware,
       dvmService.middleware,
       encounterService.middleware,
