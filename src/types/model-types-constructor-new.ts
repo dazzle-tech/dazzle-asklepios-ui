4bdf083aec6a734c181613c2da50f8d7a562e8e5
hanan253yahya <165596621+hanan253yahya@users.noreply.github.com>
2025-11-22 12:00:08 +0200

    Patient Hippa

--- src/types/model-types-constructor-new.ts
+++ src/types/model-types-constructor-new.ts
@@ -733,18 +733,23 @@ export const newVisitDuration: modelTypes.VisitDuration = {
 export const newPatient: modelTypes.Patient = {
   id: undefined,
   mrn: '',
+
   firstName: '',
   secondName: '',
   thirdName: '',
   lastName: '',
+
   sexAtBirth: null,
   dateOfBirth: null,
+
   patientClasses: '',
   isPrivatePatient: false,
+
   firstNameSecondaryLang: '',
   secondNameSecondaryLang: '',
   thirdNameSecondaryLang: '',
   lastNameSecondaryLang: '',
+
   primaryMobileNumber: '',
   receiveSms: false,
   secondMobileNumber: '',
@@ -753,10 +758,12 @@ export const newPatient: modelTypes.Patient = {
   email: '',
   receiveEmail: false,
   preferredWayOfContact: null,
+
   nativeLanguage: '',
   emergencyContactName: '',
   emergencyContactRelation: '',
   emergencyContactPhone: '',
+
   role: '',
   maritalStatus: '',
   nationality: '',
@@ -765,13 +772,16 @@ export const newPatient: modelTypes.Patient = {
   occupation: '',
   responsibleParty: '',
   educationalLevel: '',
+
   previousId: '',
   archivingNumber: '',
+
   details: '',
   isUnknown: false,
+
   isVerified: false,
   isCompletedPatient: false,
-  securityAccessLevel:null,
+
   createdBy: '',
   createdDate: null,
   lastModifiedBy: null,
@@ -790,19 +800,6 @@ export const newAddress: modelTypes.Address = {
   additionalAddressLine: '',
   countryId: '',
   isCurrent: true,
-  createdBy: '',
-  createdDate: null,
-  lastModifiedBy: null,
-  lastModifiedDate: null
-};
-
-export const newPatientPreferredHealthProfessional: modelTypes.PatientPreferredHealthProfessional = {
-  id: undefined,
-  patientId: 0,
-  practitionerId: 0,
-  facilityId: 0,
-  networkAffiliation: '',
-  relatedWith: '',
   createdBy: '',
   createdDate: null,
   lastModifiedBy: null,
