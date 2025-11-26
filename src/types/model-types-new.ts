a4de23a82199d14951f0e8d027756ef80e2920db
Qaisnfi7 <qais.farouk@gmail.com>
2025-11-26 09:51:31 +0200

    patient documents v1

--- src/types/model-types-new.ts
+++ src/types/model-types-new.ts
@@ -1417,3 +1417,16 @@ export interface PatientDocument {
   lastModifiedBy?: string | null;
   lastModifiedDate?: Date | null;
 }
+
+export interface PatientDocument {
+  id?: number;
+  patientId: number; // FK -> Patient
+  countryId: number; // FK -> Country
+  category?: string | null; // PRIMARY / SECONDARY
+  type: string; // NATIONAL_ID / PASSPORT / ...
+  number: string; // Document number
+  createdBy?: string | null;
+  createdDate?: Date | null;
+  lastModifiedBy?: string | null;
+  lastModifiedDate?: Date | null;
+}
