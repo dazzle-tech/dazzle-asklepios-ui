a3010bffee3e67f583cbda508bfe19079038f61a
Rawan-Yahya200 <rawanyahya300@gmail.com>
2025-12-04 16:44:24 +0200

    Features/ASK-626

--- src/App.tsx
+++ src/App.tsx
@@ -213,12 +213,6 @@ const App = () => {
   const tenantQueryResponse = useLoadTenantQuery(config.tenantId);
 
   const [navigationMap, setNavigationMap] = useState<any[]>([]);
-  useEffect(() => {
-    console.log("navigationMap");
-  console.log(navigationMap);
-  },[navigationMap]);
-  
-
   const user = JSON.parse(localStorage.getItem('user') || 'null');
   const tenant = JSON.parse(localStorage.getItem('tenant') || 'null');
   const selectedFacility = tenant?.selectedFacility || null;
@@ -325,8 +319,6 @@ const App = () => {
           children: childrenNavs
         });
       }
-      console.log("childrenNavs");
-      console.log(childrenNavs);
     });
 
     setNavigationMap(navsTemp);
