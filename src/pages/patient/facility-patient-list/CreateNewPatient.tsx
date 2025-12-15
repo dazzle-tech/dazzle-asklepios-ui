51b618a95ff6308137ce8674aec0a3b42d6a2fc1
hanan253yahya <165596621+hanan253yahya@users.noreply.github.com>
2025-12-15 08:40:08 +0200

    eidt payor plan import in patient

--- src/pages/patient/facility-patient-list/CreateNewPatient.tsx
+++ src/pages/patient/facility-patient-list/CreateNewPatient.tsx
@@ -21,7 +21,7 @@ import { setRefetchEncounter } from '@/reducers/refetchEncounterState';
 
 // NEW IMPORTS for Payors / Plans
 import { useGetAllPayorsQuery } from '@/services/setup/payer/PayorService';
-import { useGetPayorPlansByPayorQuery } from '@/services/setup/payer/PayorPlanService';
+import { useGetPlansByPayorQuery } from '@/services/setup/payer/PayorPlanService';
 
 const CreateNewPatient = ({ open, setOpen }) => {
   const dispatch = useAppDispatch();
@@ -76,7 +76,7 @@ const CreateNewPatient = ({ open, setOpen }) => {
     data: plansResponse,
     isLoading: plansLoading,
     isFetching: plansFetching,
-  } = useGetPayorPlansByPayorQuery(
+  } = useGetPlansByPayorQuery(
     {
       payorId: Number(patientInsurance?.insuranceProviderLkey) || 0,
       page: planPage,
