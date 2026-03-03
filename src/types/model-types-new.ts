4954437374b29f20c346a1a24af85d2616178e23
Faroukdhelea <fdhelea@gmail.com>
2026-03-03 12:20:17 +0200

    ASK-1147

--- src/types/model-types-new.ts
+++ src/types/model-types-new.ts
@@ -3522,3 +3522,34 @@ export interface RelationsMatrix {
   firstRelationCode: string;
   secondRelationCode: string;
 }
+export interface PatientAdministrativeWarningsResponseVM {
+  id: number;
+  patientId: number;
+  warningTypeLkey: string;
+  warningTypeDisplay?: string;
+  description?: string;
+  isValid: boolean;
+
+  createdAt?: string;
+  createdBy?: string;
+
+  dateResolved?: string;
+  resolvedBy?: string;
+
+  resolutionUndoDate?: string;
+  resolvedUndoBy?: string;
+}
+
+export interface PatientAdministrativeWarningsCreateDTO {
+  patientId: number;
+  warningType: string;
+  description?: string;
+}
+
+export interface PatientAdministrativeWarningsResolveDTO {
+  id: number;
+}
+
+export interface PatientAdministrativeWarningsUndoResolveDTO {
+  id: number;
+}
