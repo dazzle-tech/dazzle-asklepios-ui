b7640116143a1d8980a29e872f793ffee6d4719b
hanan253yahya <165596621+hanan253yahya@users.noreply.github.com>
2026-03-04 15:28:28 +0200

    LV

--- src/types/model-types-constructor-new.ts
+++ src/types/model-types-constructor-new.ts
@@ -2103,45 +2103,7 @@ export const newNextOfKin: modelTypes.NextOfKin = {
   internationalNumber: null,
   landlineNumber: null
 };
-export const newEncounterAssessment: modelTypes.EncounterAssessment = {
-  id: undefined,
-  patientId: null,
-  userId: null,
-  encounterId: null,
-  assessment: '',
 
-  createdBy: '',
-  createdDate: null,
-  lastModifiedBy: null,
-  lastModifiedDate: null
-};
-
-export const newEncounterPlan: modelTypes.EncounterPlan = {
-  id: undefined,
-  patientId: null,
-  encounterId: null,
-  planInstructions: '',
-
-  createdBy: '',
-  createdDate: null,
-  lastModifiedBy: null,
-  lastModifiedDate: null
-};
-
-export const newPatientDiagnosis: modelTypes.PatientDiagnosis = {
-  id: undefined,
-  patientId: null,
-  encounterId: null,
-  diagnosisId: null,
-  type: null,
-  suspected: false,
-  major: false,
-
-  createdBy: '',
-  createdDate: null,
-  lastModifiedBy: null,
-  lastModifiedDate: null
-};
 export const newEncounterAssessment: modelTypes.EncounterAssessment = {
   id: undefined,
   patientId: null,
@@ -2181,7 +2143,7 @@ export const newPatientDiagnosis: modelTypes.PatientDiagnosis = {
   lastModifiedBy: null,
   lastModifiedDate: null,
 };
-export const newVitalSigns: VitalSigns = {
+export const newVitalSigns: modelTypes.VitalSigns = {
   id: undefined,
  
   patientId: 0,
