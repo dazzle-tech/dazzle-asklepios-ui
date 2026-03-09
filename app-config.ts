d4481737f0fee622754cc76033f8aaca510892fa
Batool Sbaih <148275228+BatoolSbaih@users.noreply.github.com>
2026-03-09 14:54:12 +0200

    fix app config

--- app-config.ts
+++ app-config.ts
@@ -1,5 +1,6 @@
 const config = {
-backendBaseURL:'http://asklepiosapi.nereuscloud.de',
+    
+  backendBaseURL:'http://asklepiosapi.nereuscloud.de',
  // backendBaseURL:'http://localhost:8080',
 
      tenantId: '1',
