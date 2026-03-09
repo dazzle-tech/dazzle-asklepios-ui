18b1a985cbb8d6c6624c0a3aaacccbf617045037
Rawan-Yahya200 <rawanyahya300@gmail.com>
2026-03-09 14:33:06 +0200

    Features/ASK-1156 previous visit

--- src/pages/encounter/encounter-component/patient-summary/PreviuosVisitData/PreviuosVisitData.tsx
+++ src/pages/encounter/encounter-component/patient-summary/PreviuosVisitData/PreviuosVisitData.tsx
@@ -1,102 +1,37 @@
-import React, { useState, useEffect } from 'react';
-import { Divider, Form } from 'rsuite';
+import React from 'react';
+import { Form } from 'rsuite';
 import MyInput from '@/components/MyInput';
-import { ApEncounter } from '@/types/model-types';
-import { newApEncounter } from '@/types/model-types-constructor';
-import { useGetLovValuesByCodeQuery } from '@/services/setupService';
-import { useGetPatientDiagnosisQuery } from '@/services/encounterService';
-import { useGetEncountersQuery } from '@/services/encounterService';
-import { initialListRequest, ListRequest } from '@/types/types';
 import Section from '@/components/Section';
 import { useGetPreviousClosedEncounterQuery } from '@/services/encounters/patientEncounterService';
 import { useEnumOptions } from '@/services/enumsApi';
+import { useGetPrimaryByEncounterIdQuery } from '@/services/medicalsheetsEncounter/clinicalVisit/patientDiagnosisService';
+import { useGetIcdDiagnosisByIdQuery } from '@/services/setup/icdTreeService';
 const PreviuosVisitData = ({ patient, encounter }) => {
-  const [prevencounter, setPrevencounter] = useState<ApEncounter>({
-    ...newApEncounter,
-    discharge: false
-  });
+  
 
   const {
   data: previousEncounter,
-  isFetching
 } = useGetPreviousClosedEncounterQuery(
   { encounterId: encounter?.id as number },
   { skip: !encounter?.id }
 );
-console.log("previousEncounter: ", previousEncounter);
 
-  const  encounterReasonEnumQueryResponse = useEnumOptions('EncounterReason');
-  const { data: encounterReasonLovQueryResponse } = useGetLovValuesByCodeQuery('ENC_REASON');
-  const [patientVisitListRequest, setPatientVisitListReques] = useState<ListRequest>({
-    ...initialListRequest,
-
-    sortBy: 'plannedStartDate',
-    sortType: 'desc',
-    filters: [
-      {
-        fieldName: 'patient_key',
-        operator: 'match',
-        value: patient?.key
-      },
-      {
-        fieldName: 'planned_start_date',
-        operator: 'lt',
-        value: encounter?.plannedStartDate
-      }
-    ]
-  });
-  const [listdRequest, setListdRequest] = useState({
-    ...initialListRequest,
-    pageSize: 100,
-    timestamp: new Date().getMilliseconds(),
-    sortBy: 'createdAt',
-    sortType: 'desc',
-    filters: [
-      {
-        fieldName: 'patient_key',
-        operator: 'match',
-        value: patient?.key
-      },
-      {
-        fieldName: 'visit_key',
-        operator: 'match',
-        value: prevencounter?.key
-      }
-    ]
-  });
-  const { data: Diagnoses, refetch: fetchlastDiag } = useGetPatientDiagnosisQuery(listdRequest, {
-    refetchOnMountOrArgChange: true,
-    refetchOnFocus: true
-  });
-  const { data: encounterPatientList } = useGetEncountersQuery(
-    { ...patientVisitListRequest },
-    {
-      refetchOnMountOrArgChange: true,
-      refetchOnFocus: true
-    }
+const {
+    data: primaryDiagnosisForPreviousVisit,
+  } = useGetPrimaryByEncounterIdQuery(
+    { encounterId: previousEncounter?.id as number },
+    { skip: !previousEncounter?.id }
   );
 
-  useEffect(() => {
-    setPrevencounter(encounterPatientList?.object[0]);
-  }, [encounterPatientList]);
-  useEffect(() => {
-    const updatedFilters = [
-      {
-        fieldName: 'patient_key',
-        operator: 'match',
-        value: patient?.key
-      },
-      {
-        fieldName: 'visit_key',
-        operator: 'match',
-        value: prevencounter?.key
-      }
-    ];
-    setListdRequest(prevRequest => ({
-      ...prevRequest,
-      filters: updatedFilters
-    }));
-  }, [prevencounter]);
+  const {
+    data: diagnosisObject,
+  } = useGetIcdDiagnosisByIdQuery(
+    { id: primaryDiagnosisForPreviousVisit?.diagnosisId as number },
+    { skip: !primaryDiagnosisForPreviousVisit?.diagnosisId }
+  );
+  
+  const  encounterReasonEnumQueryResponse = useEnumOptions('EncounterReason');
+  
   return (
     <Section
       title="Previous Visit"
@@ -127,8 +62,8 @@ console.log("previousEncounter: ", previousEncounter);
             column
             width={140}
             fieldLabel="Diagnosis Description"
-            fieldName="description"
-            record={Diagnoses?.object[0]?.diagnosisObject || {}}
+            fieldName="icdShortDescription"
+            record={diagnosisObject || {}}
           />
         </Form>
       }
