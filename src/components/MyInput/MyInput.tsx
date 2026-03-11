cacb688c2b58ba3f93d90ee422e309ab322f4a5a
Qaisnfi7 <qais.farouk@gmail.com>
2026-03-11 14:08:30 +0200

    ask 1273,1266,1286,1287,1277

--- src/components/MyInput/MyInput.tsx
+++ src/components/MyInput/MyInput.tsx
@@ -695,21 +695,8 @@ const MyInput = ({
             format="dd-MM-yyyy"
             editable
             cleanable={false}
+            oneTap
             defaultValue={record?.[fieldName] ? dayjs(record[fieldName]).toDate() : null}
-            inputRef={(ref: HTMLInputElement) => {
-              if (!ref) return;
-
-              const handleFocus = () => {
-                setTimeout(() => {
-                  if (!ref.value) {
-                    ref.value = '__-__-____'.replace(/_/g, '');
-                    ref.setSelectionRange(0, 2);
-                  }
-                }, 0);
-              };
-
-              ref.addEventListener('focus', handleFocus);
-            }}
             onChange={(value: Date | null) => {
               const dateStr = value ? dayjs(value).format('YYYY-MM-DD') : null;
               setRecord?.({ ...record, [fieldName]: dateStr });
@@ -720,11 +707,9 @@ const MyInput = ({
 
               if (e.ctrlKey && e.key.toLowerCase() === 'a') {
                 e.preventDefault();
-
                 setTimeout(() => {
                   input.setSelectionRange(0, 2);
                 }, 0);
-
                 return;
               }
 
