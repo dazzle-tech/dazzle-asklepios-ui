905458c5b17877c3657944287b98b8f9cd4d2489
Rawan-Yahya200 <rawanyahya300@gmail.com>
2026-03-25 12:22:23 +0200

    Features/ASK-1379

--- src/pages/system-configurations/facilities-setup/AddEditFacility.tsx
+++ src/pages/system-configurations/facilities-setup/AddEditFacility.tsx
@@ -1,7 +1,7 @@
 import MyModal from '@/components/MyModal/MyModal';
 import React, { useEffect, useState } from 'react';
 import MyInput from '@/components/MyInput';
-import { Form } from 'rsuite';
+import { Col, Form, Row } from 'rsuite';
 import { useGetLovValuesByCodeQuery } from '@/services/setupService';
 import { useGetLovValuesByCodeAndParentQuery } from '@/services/setupService';
 import { faLocationDot } from '@fortawesome/free-solid-svg-icons';
@@ -22,10 +22,10 @@ const AddEditFacility = ({
   handleSave
 }) => {
   const [validationResult] = useState({});
-  
+
 
   // Fetch  facilityTypeOptions list response
-  const facilityTypeOptions = useEnumOptions("FacilityType"); 
+  const facilityTypeOptions = useEnumOptions("FacilityType");
 
   const currencyOptions = useEnumCapitalized("Currency");
   // Fetch country Lov list response
@@ -37,6 +37,7 @@ const AddEditFacility = ({
     code: 'CITY',
     parentValueKey: address?.countryLkey
   });
+  const timeZone = useEnumOptions('TimeZone');
 
   // modal content
   const conjureFormContent = stepNumber => {
@@ -44,22 +45,20 @@ const AddEditFacility = ({
       case 0:
         return (
           <Form fluid >
-           <div
-              className={clsx('', {
-                'container-of-two-fields-facility': width > 600
-              })}
-            >
-            
-            <MyInput
-              fieldName="name"
-              record={facility}
-              setRecord={setFacility}
-              width={"16vw"}
-              required
-            />
+           <Row>
+            <Col md={12}>
+              <MyInput
+                fieldName="name"
+                record={facility}
+                setRecord={setFacility}
+                width="100%"
+                required
+              />
+              </Col>
+              <Col md={12}>
               <MyInput
                 required
-                width={"13vw"}
+                width="100%"
                 vr={validationResult}
                 fieldLabel="Facility Type"
                 fieldType="select"
@@ -71,16 +70,13 @@ const AddEditFacility = ({
                 record={facility}
                 setRecord={setFacility}
               />
-         
-            </div>
-            <div
-              className={clsx('', {
-                'container-of-two-fields-facility': width > 600
-              })}
-            >
-                   <MyInput
+              </Col>
+               </Row>
+            <Row>
+              <Col md={12}>
+              <MyInput
                 required
-                width={"13vw"}
+                width="100%"
                 vr={validationResult}
                 fieldLabel="Default Currency"
                 fieldType="select"
@@ -92,22 +88,38 @@ const AddEditFacility = ({
                 setRecord={setFacility}
                 searchable={false}
               />
+              </Col>
+              <Col md={12}>
               <MyInput
                 fieldName="registrationDate"
                 fieldType="date"
                 record={facility}
                 setRecord={setFacility}
-                width={"13vw"}
+                width="100%"
               />
-         
-            </div>
+              </Col>
+            </Row>
+            <Row>
+            <MyInput
+              fieldName="timeZone"
+              fieldType="select"
+              selectData={timeZone ?? []}
+              selectDataLabel="label"
+              selectDataValue="value"
+              record={facility}
+              setRecord={setFacility}
+              width={"100%"}
+            />
+            </Row>
+             <Row>
             <MyInput
               fieldName="facilityBriefDesc"
               fieldType="textarea"
               record={facility}
               setRecord={setFacility}
-              width={"26vw"}
+              width={"100%"}
             />
+            </Row>
           </Form>
         );
       case 1:
@@ -226,10 +238,10 @@ const AddEditFacility = ({
       actionButtonLabel={facility?.id ? 'Save' : 'Create'}
       actionButtonFunction={handleSave}
       size="38vw"
-        steps={[
-        { title: 'Basic Info', icon:<FontAwesomeIcon icon={ faUser }/>},
-        { title: 'Address', icon:<FontAwesomeIcon icon={ faLocationDot }/>},
-        { title: 'Contact', icon: <FontAwesomeIcon icon={faPhone }/>}
+      steps={[
+        { title: 'Basic Info', icon: <FontAwesomeIcon icon={faUser} /> },
+        { title: 'Address', icon: <FontAwesomeIcon icon={faLocationDot} /> },
+        { title: 'Contact', icon: <FontAwesomeIcon icon={faPhone} /> }
       ]}
     />
   );
