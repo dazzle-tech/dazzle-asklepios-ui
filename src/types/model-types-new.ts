ddba6876d56644ff2343dc6d2e3717216c758cc9
Qaisnfi7 <qais.farouk@gmail.com>
2026-04-01 11:14:28 +0300

    ask-1360,1472

--- src/types/model-types-new.ts
+++ src/types/model-types-new.ts
@@ -3810,3 +3810,8 @@ export interface NurseSummaryReportVM {
   servicesAndProducts: NurseSummaryServiceProductVM[];
   generatedAt: string | null;
 }
+
+export type PatientDiagnosisFlag = {
+  encounterId: number;
+  hasPrimaryDiagnoses: boolean;
+};
