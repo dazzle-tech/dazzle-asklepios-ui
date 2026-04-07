04564053e54583838b7c38fcfafa2c2c64c5fabe
Batool Sbaih <148275228+BatoolSbaih@users.noreply.github.com>
2026-04-07 12:57:56 +0300

    Fix/ask 1497 (#1972)
    * Merge branch 'main' of https://github.com/dazzle-tech/dazzle-asklepios-ui
    
    * LV

--- src/pages/encounter/encounter-component/prescription-new/DetailsModal.tsx
+++ src/pages/encounter/encounter-component/prescription-new/DetailsModal.tsx
@@ -1,6 +1,5 @@
 import AdvancedModal from '@/components/AdvancedModal';
 import Icd10DiagnosisSearch from '@/components/Icd10DiagnosisSearch';
-import InfoCardList from '@/components/InfoCardList';
 import MyButton from '@/components/MyButton/MyButton';
 import MyInput from '@/components/MyInput';
 import MyLabel from '@/components/MyLabel';
@@ -21,17 +20,13 @@ import {
   useSearchBrandMedicationsByNameOrActiveQuery
 } from '@/services/setup/brandmedication/BrandMedicationService';
 import './styles.less';
-import SectionContainer from '@/components/SectionsoContainer';
 import { AttachmentUploadModal } from '@/components/AttachmentModals';
 import { conjureValueBasedOnKeyFromList } from '@/utils';
-import { useEnumOptions } from '@/services/enumsApi';
-import { useLazyGetActiveIngredientPreRequestedTestsQuery } from '@/services/setup/activeIngredients/activeIngredientPreRequestedTestService';
 import InfoCardList from '@/components/InfoCardList';
 import { useGetAllDiagnosticTestsQuery } from '@/services/setup/diagnosticTest/diagnosticTestService';
 import { useGetIcdListQuery, useGetLovValuesByCodeQuery } from '@/services/setupService';
 import { newApPrescriptionMedications } from '@/types/model-types-constructor';
 import { initialListRequest, ListRequest } from '@/types/types';
-import { conjureValueBasedOnKeyFromList } from '@/utils';
 import { notify } from '@/utils/uiReducerActions';
 import { faRightLeft } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -43,8 +38,6 @@ import DiagnosticsOrder from '../diagnostics-order-new';
 import Substitues from '../drug-order/SubstitutesNew';
 import ActiveIngrediantList from './ActiveIngredient';
 import Instructions from './Instructions';
-import './styles.less';
-
 const DetailsModal = ({
   edit,
   open,
@@ -1164,4 +1157,5 @@ const DetailsModal = ({
     </div>
   );
 };
+
 export default DetailsModal;
