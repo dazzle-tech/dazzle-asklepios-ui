a09fb2c78273a500bec461a852b9c2ebde8f9081
Faroukdhelea <fdhelea@gmail.com>
2026-04-13 15:24:29 +0300

    New Modal Edits

--- src/pages/encounter/encounter-component/ucc-medication-order/UccMedicationOrderAddModal.tsx
+++ src/pages/encounter/encounter-component/ucc-medication-order/UccMedicationOrderAddModal.tsx
@@ -8,6 +8,7 @@ import { notify } from '@/utils/uiReducerActions';
 import { Form, RadioGroup, Radio, Row, Col } from 'rsuite';
 import { useGetLovValuesByCodeQuery } from '@/services/setupService';
 import './styles.less';
+import Instructions from '../prescription-new/Instructions';
 
 const UccMedicationOrderAddModal = ({
   open,
