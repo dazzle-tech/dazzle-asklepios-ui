7f3dc7b678a9087f34a94ed43811271b589bab87
hanan253yahya <165596621+hanan253yahya@users.noreply.github.com>
2026-04-19 15:47:39 +0300

    1716 (#2054)

--- src/services/setupService.ts
+++ src/services/setupService.ts
@@ -180,6 +180,23 @@ export const setupService = createApi({
       onQueryStarted: onQueryStarted,
       keepUnusedDataFor: 0
     }),
+    getLovValuesByKey: builder.query({
+  query: (lovKey: string) => ({
+    url: `/setup/lov-value-list/by-key?key=${lovKey}`
+  }),
+  
+  onQueryStarted: onQueryStarted,
+  keepUnusedDataFor: 0
+}),
+getLovValuesBulkByKeys: builder.query({
+  query: (lovKeys: string[]) => ({
+    url: `/setup/lov-value-list/bulk-by-keys?${lovKeys
+      .map(key => `lov_keys=${key}`)
+      .join("&")}`
+  }),
+  onQueryStarted: onQueryStarted,
+  keepUnusedDataFor: 0
+}),
     saveLovValue: builder.mutation({
       query: (lovValue: ApLovValues) => ({
         url: `/setup/save-lov-value`,
@@ -1632,5 +1649,8 @@ export const {
   useSaveWarehouseProductsDetailsMutation,
   useRemoveWarehouseProductsDetailsMutation,
   useLazyGetCandidatesByDepartmentKeyQuery,
- useGetIcdByIdQuery
+ useGetIcdByIdQuery,
+ useGetLovValuesByKeyQuery,
+ useGetLovValuesBulkByKeysQuery,
+ useLazyGetLovValuesBulkByKeysQuery,
 } = setupService;
