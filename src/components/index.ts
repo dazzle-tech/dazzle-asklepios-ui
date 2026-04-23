191953d5a83d59413e4613285e49baebdbd4bed5
sondosabdalaal12 <engsondosabdalaal@outlook.com>
2026-04-23 15:23:09 +0300

    phone number

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as ErrorPage } from './ErrorPage';
 export { default as Header } from './Header';
 export { default as Frame } from './Frame';
+export { default as PhoneNumberInput } from './PhoneNumberInput';
