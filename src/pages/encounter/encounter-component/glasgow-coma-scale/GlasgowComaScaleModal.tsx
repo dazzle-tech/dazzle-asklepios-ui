a109ddb04149dc3f6cfb9a6ab2f2c4b5fba7daf2
Batool Sbaih <148275228+BatoolSbaih@users.noreply.github.com>
2026-05-10 16:18:32 +0300

    LV

--- src/pages/encounter/encounter-component/glasgow-coma-scale/GlasgowComaScaleModal.tsx
+++ src/pages/encounter/encounter-component/glasgow-coma-scale/GlasgowComaScaleModal.tsx
@@ -7,6 +7,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faG } from '@fortawesome/free-solid-svg-icons';
 import { useEnumOptions } from '@/services/enumsApi';
 import './Style.less';
+import { formatEnumString } from '@/utils';
 
 const getGcsScore = (
   type: 'eye' | 'verbal' | 'motor',
