c1ed28983a72d3fbacea841c1476e14b3155e703
Batool Sbaih <148275228+BatoolSbaih@users.noreply.github.com>
2026-05-10 15:42:31 +0300

    ASK 1824 (#2157)
    * ASK 1824
    
    * LV

--- src/pages/patient/patient-profile/InsuranceModal.tsx
+++ src/pages/patient/patient-profile/InsuranceModal.tsx
@@ -107,7 +107,7 @@ const InsuranceModal = ({
   const [patientInsurance, setPatientInsurance] = useState<PatientInsurance>({
     ...newPatientInsurance
   });
-
+console.log('Resolved Patient ID:', resolvedPatientId);
   const [addPatientInsurance] = useAddPatientInsuranceMutation();
   const [updatePatientInsurance] = useUpdatePatientInsuranceMutation();
 
