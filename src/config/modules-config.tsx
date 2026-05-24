99996eaabea5d998399b24e996cbfa4895ce3df6
bushramashaiekh <bushramashaie5@gmail.com>
2026-05-24 10:13:57 +0300

    Update modules-config.tsx

--- src/config/modules-config.tsx
+++ src/config/modules-config.tsx
@@ -717,6 +717,16 @@ export const MODULES: Module[] =
 
       ],
     },
+    {
+      name: "Waseel Integration",
+      description: null,
+      icon: "FaRegIdBadge",
+      viewOrder: 2,
+      screens: [
+
+        { name: "Waseel Pre Authorization Requests", code: "WASEEL_PRE_AUTHORIZATION_REQUESTS", description: "", icon: "FaFilePen", viewOrder: 1, navPath: "" },
+  ],
+    },
 
 
 
