c8575d46914468a71c479b23f9e260183b07186e
hanan253yahya <165596621+hanan253yahya@users.noreply.github.com>
2026-06-08 13:28:29 +0300

    test

--- app-config.ts
+++ app-config.ts
@@ -1,9 +1,23 @@
-const config = {
-  backendBaseURL: 'http://asklepiosapi.nereuscloud.de',
-    // backendBaseURL:'http://localhost:8080',
+declare global {
+  interface Window {
+    APP_CONFIG?: {
+      backendBaseURL?: string;
+      tenantId?: string;
+      tenantSecurityToken?: string;
+    };
+  }
+}
 
-  tenantId: '1',
-  tenantSecurityToken: '4994'
+const config = {
+  backendBaseURL:
+    window.APP_CONFIG?.backendBaseURL || 'http://asklepiosapi.nereuscloud.de',
+  tenantId:
+    window.APP_CONFIG?.tenantId || '1',
+  tenantSecurityToken:
+    window.APP_CONFIG?.tenantSecurityToken || '4994'
 };
+
+console.log('APP_CONFIG', window.APP_CONFIG);
+console.log('CONFIG', config);
 
 export default config;
---
+++ public/config.js
@@ -0,0 +1,5 @@
+window.APP_CONFIG = {
+  backendBaseURL: "http://localhost:8080",
+  tenantId: "1",
+  tenantSecurityToken: "4994"
+}; 
