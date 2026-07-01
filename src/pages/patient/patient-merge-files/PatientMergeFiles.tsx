493b9c33e0e45bdbb7cc64bdc232f9d4bad6962e
hanan253yahya <165596621+hanan253yahya@users.noreply.github.com>
2026-07-01 13:53:00 +0300

    edit prescription

--- src/pages/patient/patient-merge-files/PatientMergeFiles.tsx
+++ src/pages/patient/patient-merge-files/PatientMergeFiles.tsx
@@ -103,7 +103,6 @@ const PatientMergeFiles: React.FC = () => {
       };
 
       const result = await executeMerge(executeRequest).unwrap();
-      console.log("RESULT", result)
       dispatch(notify({
         msg: `Merge completed successfully. Log Number : ${result.transactionNumber || 'N/A'}`,
         sev: 'success'
