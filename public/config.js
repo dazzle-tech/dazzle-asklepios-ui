3204acba84b948771712c6d85c7bf2ab3fe37e20
sondosabdalaal12 <engsondosabdalaal@outlook.com>
2026-09-10 08:38:25 +0300

    license

--- public/config.js
+++ public/config.js
@@ -1,8 +1,15 @@
 window.APP_CONFIG = {
- backendBaseURL: "http://localhost:8080",
-    // backendBaseURL: "http://asklepiosapi.nereuscloud.de",
+ // backendBaseURL: "http://localhost:8080",
+    backendBaseURL: "http://asklepiosapi.nereuscloud.de",
 
   tenantId: "1",
   tenantSecurityToken: "4994",
-  stimulsoftProxyUrl: ""
+  stimulsoftProxyUrl: "",
+  stimulsoftLicenseKey: "6vJhGtLLLz2GNviWmUTrhSqnOItdDwjBylQzQcAOiHn9ra6B7L/WLSvNB/6pzc7v23PZ0FDX1CxOXv/jmPZjt0hLNd" + 
+"gRVVXrajiYKwq8N4JrxS3VOz8v52E38H1HDHamyp04wwrktq7m1QAvDKtcjQ/hErt2mG/9AbHRx6usSpL206racpe2" + 
+"6sWMZPn7HxV1YMQNab5XZHdvK9jiAS9mLOP+YizvLc8DV9xYjU5fKGQPlHX3Ja0EsgQe9mfNpdbTGoG5Dl7q2SWoo/" + 
+"KcweZ4kXu9kT4L59j4SjQ+bRIoNc6foOdC9Rd49av6X5+UPyBQP8akd9z9pr0hSXUgZpLbvWB5ftw/3kvrKGxSkh0Z" + 
+"d8urxo8cW+y1iKSuuHHqNEH48yW+GsJLizWyN2EliH9JFA6Eef29YvXLRxAaKn5si/01gfm0cYokNl480M2ChM7g4b" + 
+"G5ybeNA24iY9ZABWrGZVEV4lW3kkeIh+gpOUFI46QCuDHQh73qf69QtNnts1FTWLTx93pSe4KWKV0kUyXUPUI5jyzz" + 
+"iBvtJ3YqRWQ9E6/cVFWgcELoJsLUnQr0qEF3"
 }; 
