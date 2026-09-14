5ac21684d3903076a0767551dde4ff1a3a4b1551
Faroukdhelea <fdhelea@gmail.com>
2026-09-14 14:26:15 +0300

    ASK-2712

--- src/types/model-types-new.ts
+++ src/types/model-types-new.ts
@@ -5068,53 +5068,95 @@ export type CurrentMedicationForm = CurrentMedication & {
 };
 
 
-export interface PatientUccMedicationOrder {
-  id?: number;
+  export interface PatientUccMedicationOrder {
+    id?: number;
 
-  patientId: number;
-  encounterId: number;
+    patientId: number;
+    encounterId: number;
 
-  activeIngredientId: number;
+    activeIngredientId: number;
 
-  instructionType: 'MANUAL_INSTRUCTIONS' | 'CUSTOM_INSTRUCTIONS';
-  instructionText?: string | null;
+    instructionType: 'MANUAL_INSTRUCTIONS' | 'CUSTOM_INSTRUCTIONS';
+    instructionText?: string | null;
 
-  dose?: number | null;
-  doseUnit?: string | null;
-  route?: string | null;
-  frequency?: string | null;
+    dose?: number | null;
+    doseUnit?: string | null;
+    route?: string | null;
+    frequency?: string | null;
 
-  isHighAlert?: boolean | null;
+    isHighAlert?: boolean | null;
 
-  status?:
-  | 'WAITING_DOUBLE_CHECK'
-  | 'ADMINISTERED'
-  | 'CANCELLED'
-  | 'DISCARDED'
-  | 'NEW'
-  | 'SUBMITTED';
+    status?:
+    | 'WAITING_DOUBLE_CHECK'
+    | 'ADMINISTERED'
+    | 'CANCELLED'
+    | 'DISCARDED'
+    | 'NEW'
+    | 'SUBMITTED';
 
-  submittedDate?: string | Date | null;
-  submittedBy?: string | null;
+    submittedDate?: string | Date | null;
+    submittedBy?: string | null;
 
-  administeredDate?: string | Date | null;
-  administeredBy?: string | null;
+    administeredDate?: string | Date | null;
+    administeredBy?: string | null;
 
-  doubleCheckedDate?: string | Date | null;
-  doubleCheckedBy?: string | null;
+    doubleCheckedDate?: string | Date | null;
+    doubleCheckedBy?: string | null;
 
-  discardedDate?: string | Date | null;
-  discardedBy?: string | null;
-  discardReason?: string | null;
+    discardedDate?: string | Date | null;
+    discardedBy?: string | null;
+    discardReason?: string | null;
 
-  cancelledDate?: string | Date | null;
-  cancelledBy?: string | null;
-  cancellationReason?: string | null;
-  createdBy?: string | null;
-  createdDate?: string | Date | null;
-  lastModifiedBy?: string | null;
-  lastModifiedDate?: string | Date | null;
-}
+    cancelledDate?: string | Date | null;
+    cancelledBy?: string | null;
+    cancellationReason?: string | null;
+    createdBy?: string | null;
+    createdDate?: string | Date | null;
+    lastModifiedBy?: string | null;
+    lastModifiedDate?: string | Date | null;
+  }
+
+  export interface PatientUccMedicationOrderGroup {
+    orderGroupId: number;
+
+    patientId: number;
+    encounterId: number;
+    activeIngredientId: number;
+
+    instructionType: 'MANUAL_INSTRUCTIONS' | 'CUSTOM_INSTRUCTIONS';
+
+    instructionText?: string | null;
+
+    dose?: number | null;
+    doseUnit?: string | null;
+    route?: string | null;
+
+    frequencyNumber?: number | null;
+    frequencyUnit?: string | null;
+    duration?: number | null;
+    startTime?: string | null;
+
+    doseCount: number;
+
+    isHighAlert?: boolean | null;
+
+    status?:
+      | 'WAITING_DOUBLE_CHECK'
+      | 'ADMINISTERED'
+      | 'CANCELLED'
+      | 'DISCARDED'
+      | 'NEW'
+      | 'SUBMITTED';
+
+    submittedDate?: string | Date | null;
+    submittedBy?: string | null;
+
+    createdBy?: string | null;
+    createdDate?: string | Date | null;
+
+    lastModifiedBy?: string | null;
+    lastModifiedDate?: string | Date | null;
+  }
 
 // ------------------- Dental Procedures -------------------
 
